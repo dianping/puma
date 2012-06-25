3cc5330b720ccec44c03b2ad41307386a353d356
jinhua.liang <jinhua.liang@dianping.com>
2012-06-26 01:20:40 +0800

    refactor

--- puma/puma-datahandler/src/main/java/com/dianping/puma/datahandler/impl/TransactionSupportDataHandler.java
+++ puma/puma-datahandler/src/main/java/com/dianping/puma/datahandler/impl/TransactionSupportDataHandler.java
@@ -31,6 +31,7 @@ import com.dianping.puma.common.mysql.BinlogConstanst;
 import com.dianping.puma.common.mysql.Row;
 import com.dianping.puma.common.mysql.UpdatedRowData;
 import com.dianping.puma.common.mysql.column.Column;
+import com.dianping.puma.common.mysql.event.AbstractRowsEvent;
 import com.dianping.puma.common.mysql.event.BinlogEvent;
 import com.dianping.puma.common.mysql.event.DeleteRowsEvent;
 import com.dianping.puma.common.mysql.event.TableMapEvent;
@@ -79,11 +80,8 @@ public class TransactionSupportDataHandler extends AbstractDataHandler {
 				List<RowChangedData> writeRowChangedDataList = new ArrayList<RowChangedData>();
 				for (Row row : writeRowsEvent.getRows()) {
 					RowChangedData rowChangedData = new RowChangedData();
-					Map<String, ColumnInfo> columns = new HashMap<String, RowChangedData.ColumnInfo>();
-					rowChangedData.setActionType(ActionType.INSERT);
-					rowChangedData.setExecuteTime(writeRowsEvent.getHeader().getTimestamp());
-					rowChangedData.setColumns(columns);
-					writeRowChangedDataList.add(rowChangedData);
+					Map<String, ColumnInfo> columns = initColumns(writeRowsEvent, writeRowChangedDataList,
+							rowChangedData, ActionType.INSERT);
 
 					for (int columnPos = 0; columnPos < writeRowsEvent.getUsedColumns().size(); columnPos++) {
 						if (writeRowsEvent.getUsedColumns().get(columnPos)) {
@@ -104,11 +102,8 @@ public class TransactionSupportDataHandler extends AbstractDataHandler {
 				List<RowChangedData> updateRowChangedDataList = new ArrayList<RowChangedData>();
 				for (UpdatedRowData<Row> row : updateRowsEvent.getRows()) {
 					RowChangedData rowChangedData = new RowChangedData();
-					Map<String, ColumnInfo> columns = new HashMap<String, RowChangedData.ColumnInfo>();
-					rowChangedData.setActionType(ActionType.UPDATE);
-					rowChangedData.setExecuteTime(updateRowsEvent.getHeader().getTimestamp());
-					rowChangedData.setColumns(columns);
-					updateRowChangedDataList.add(rowChangedData);
+					Map<String, ColumnInfo> columns = initColumns(updateRowsEvent, updateRowChangedDataList,
+							rowChangedData, ActionType.UPDATE);
 
 					for (int columnBeforePos = 0; columnBeforePos < updateRowsEvent.getUsedColumnsBefore().size(); columnBeforePos++) {
 						if (updateRowsEvent.getUsedColumnsBefore().get(columnBeforePos)) {
@@ -141,11 +136,8 @@ public class TransactionSupportDataHandler extends AbstractDataHandler {
 				List<RowChangedData> deleteRowChangedDataList = new ArrayList<RowChangedData>();
 				for (Row row : deleteRowsEvent.getRows()) {
 					RowChangedData rowChangedData = new RowChangedData();
-					Map<String, ColumnInfo> columns = new HashMap<String, RowChangedData.ColumnInfo>();
-					rowChangedData.setActionType(ActionType.DELETE);
-					rowChangedData.setExecuteTime(deleteRowsEvent.getHeader().getTimestamp());
-					rowChangedData.setColumns(columns);
-					deleteRowChangedDataList.add(rowChangedData);
+					Map<String, ColumnInfo> columns = initColumns(deleteRowsEvent, deleteRowChangedDataList,
+							rowChangedData, ActionType.DELETE);
 
 					for (int columnPos = 0; columnPos < deleteRowsEvent.getUsedColumns().size(); columnPos++) {
 						if (deleteRowsEvent.getUsedColumns().get(columnPos)) {
@@ -165,5 +157,15 @@ public class TransactionSupportDataHandler extends AbstractDataHandler {
 
 		return null;
 
+	}
+
+	private Map<String, ColumnInfo> initColumns(AbstractRowsEvent rowsEvent,
+			List<RowChangedData> writeRowChangedDataList, RowChangedData rowChangedData, ActionType actionType) {
+		Map<String, ColumnInfo> columns = new HashMap<String, ColumnInfo>();
+		rowChangedData.setActionType(actionType);
+		rowChangedData.setExecuteTime(rowsEvent.getHeader().getTimestamp());
+		rowChangedData.setColumns(columns);
+		writeRowChangedDataList.add(rowChangedData);
+		return columns;
 	}
 }

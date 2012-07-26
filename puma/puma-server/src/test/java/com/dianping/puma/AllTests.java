be04b1a0cde1be6deaa207180566445638a8d35b
jinhua.liang <jinhua.liang@dianping.com>
2012-07-26 15:24:01 +0800

    add test into all test

--- puma/puma-server/src/test/java/com/dianping/puma/AllTests.java
+++ puma/puma-server/src/test/java/com/dianping/puma/AllTests.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.dianping.puma.storage.DefaultArchiveStrategyTest;
 import com.dianping.puma.storage.LocalBucketIndexTest;
 import com.dianping.puma.storage.LocalBucketTest;
 import com.dianping.puma.storage.SequenceTest;
@@ -14,7 +15,8 @@ import com.dianping.puma.storage.SequenceTest;
 // add test classes here
 SequenceTest.class,
 LocalBucketTest.class,
-LocalBucketIndexTest.class
+LocalBucketIndexTest.class,
+DefaultArchiveStrategyTest.class
 
 })
 public class AllTests {

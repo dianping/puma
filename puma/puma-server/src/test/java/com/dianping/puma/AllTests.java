2ed4704ff7db57e1b1c711ee807e770a71966966
zhiying lin <zhiying.lin@dianping.com>
2012-07-26 10:31:44 +0800

    test unit localbucketindex

--- puma/puma-server/src/test/java/com/dianping/puma/AllTests.java
+++ puma/puma-server/src/test/java/com/dianping/puma/AllTests.java
@@ -4,10 +4,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.dianping.puma.storage.LocalBucketIndexTest;
+import com.dianping.puma.storage.LocalBucketTest;
+import com.dianping.puma.storage.SequenceTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({
+@SuiteClasses( {
 
 // add test classes here
+SequenceTest.class,
+LocalBucketTest.class,
+LocalBucketIndexTest.class
 
 })
 public class AllTests {

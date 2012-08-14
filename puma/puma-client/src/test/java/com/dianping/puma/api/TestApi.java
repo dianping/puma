9bd5d2611e4e656e90203d37facd4506b84ccfe5
jinhua.liang <jinhua.liang@dianping.com>
2012-08-14 16:27:39 +0800

    add test class

---
+++ puma/puma-client/src/test/java/com/dianping/puma/api/TestApi.java
@@ -0,0 +1,58 @@
+/**
+ * Project: puma-client
+ * 
+ * File Created at 2012-8-14
+ * $Id$
+ * 
+ * Copyright 2010 dianping.com.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Dianping Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with dianping.com.
+ */
+package com.dianping.puma.api;
+
+import com.dianping.puma.core.event.ChangedEvent;
+
+/**
+ * TODO Comment of TestApi
+ * 
+ * @author Leo Liang
+ * 
+ */
+public class TestApi {
+	public static void main(String[] args) {
+		ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+		configBuilder.ddl(true);
+		configBuilder.dml(true);
+		configBuilder.host("192.168.8.21");
+		configBuilder.port(8080);
+		configBuilder.masterUrl("192.168.7.43:3306");
+		configBuilder.name("testClient");
+		configBuilder.tables("cat", "*");
+		configBuilder.target("7-43");
+		configBuilder.transaction(true);
+		PumaClient pc = new PumaClient(configBuilder.build());
+		pc.register(new EventListener() {
+
+			@Override
+			public void onSkipEvent(ChangedEvent event) {
+				System.out.println(">>>>>>>>>>>>>>>>>>Skip " + event);
+			}
+
+			@Override
+			public boolean onException(ChangedEvent event, Exception e) {
+				return true;
+			}
+
+			@Override
+			public void onEvent(ChangedEvent event) throws Exception {
+//				System.out.println("********************Received " + event);
+			}
+		});
+		pc.start();
+	}
+}

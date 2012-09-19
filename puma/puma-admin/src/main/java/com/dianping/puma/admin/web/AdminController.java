e4d08a7777ae6c92379f6e5031274ff976995e4b
atell <atell.wu@gmail.com>
2012-09-19 10:29:42 +0800

    puma-admin 页面设计

--- puma/puma-admin/src/main/java/com/dianping/puma/admin/web/AdminController.java
+++ puma/puma-admin/src/main/java/com/dianping/puma/admin/web/AdminController.java
@@ -10,12 +10,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 public class AdminController {
    @RequestMapping(value = "/")
-   public ModelAndView allApps(HttpServletRequest request, HttpServletResponse response) {
+   public ModelAndView watch(HttpServletRequest request, HttpServletResponse response) {
       Map<String, Object> map = new HashMap<String, Object>();
       return new ModelAndView("index", map);
+   }
+
+   @RequestMapping(value = "/create")
+   public ModelAndView createSync(HttpServletRequest request, HttpServletResponse response) {
+      Map<String, Object> map = new HashMap<String, Object>();
+
+      return new ModelAndView("create", map);
    }
 }

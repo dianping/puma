f62dd2714ef34ecda218f04032c1193a8110af71
kezhu.wu <atell.wu@gmail.com>
2012-11-02 16:33:16 +0800

    完成dump和load功能。 接下来是： 暂停PumaCleint&追赶功能，重启PumaClient功能

--- puma/puma-core/src/main/java/com/dianping/puma/core/sync/DumpConfig.java
+++ puma/puma-core/src/main/java/com/dianping/puma/core/sync/DumpConfig.java
@@ -2,14 +2,18 @@ package com.dianping.puma.core.sync;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
+/**
+ * dump规则：<br>
+ * (1)会为目标数据库自动创建table，但database不自动创建。<br>
+ * (2)允许db改名，table改名。但要求table的结构(字段个数，对应字段的类型)一致，字段名可以不一致。
+ */
 public class DumpConfig {
 
     private Src src;
     private Dest dest;
 
-    private Map<String, List<String>> databaseName2TableNames;
+    private List<DumpRelation> dumpRelations;
 
     public Src getSrc() {
         return src;
@@ -27,17 +31,17 @@ public class DumpConfig {
         this.dest = dest;
     }
 
-    public Map<String, List<String>> getDatabaseName2TableNames() {
-        return databaseName2TableNames;
+    public List<DumpRelation> getDumpRelations() {
+        return dumpRelations;
     }
 
-    public void setDatabaseName2TableNames(Map<String, List<String>> databaseName2TableNames) {
-        this.databaseName2TableNames = databaseName2TableNames;
+    public void setDumpRelations(List<DumpRelation> dumpRelations) {
+        this.dumpRelations = dumpRelations;
     }
 
     @Override
     public String toString() {
-        return "DumpConfig [src=" + src + ", dest=" + dest + ", databaseName2TableNames=" + databaseName2TableNames + "]";
+        return "DumpConfig [src=" + src + ", dest=" + dest + ", dumpRelations=" + dumpRelations + "]";
     }
 
     public static class Src {
@@ -45,10 +49,9 @@ public class DumpConfig {
         private Integer port;
         private String username;
         private String password;
-        private List<String> options = Arrays.asList(new String[] { "--no-autocommit", " --disable-keys", "--extended-insert",
-                "--quick", "--add-drop-database=false", "--add-drop-table=false", "--skip-add-locks",
-                "--default-character-set=utf8", "--max_allowed_packet=16777216", " --net_buffer_length=16384", "-i",
-                "--master-data=2", "--single-transaction" });
+        private List<String> options = Arrays.asList(new String[] { "--no-autocommit", " --disable-keys", "--quick",
+                "--add-drop-database=false", "--add-drop-table=false", "--skip-add-locks", "--default-character-set=utf8",
+                "--max_allowed_packet=16777216", " --net_buffer_length=16384", "-i", "--master-data=2", "--single-transaction" });
 
         public String getHost() {
             return host;
@@ -97,7 +100,6 @@ public class DumpConfig {
         private Integer port;
         private String username;
         private String password;
-        private String optionString;
 
         public String getHost() {
             return host;
@@ -131,14 +133,48 @@ public class DumpConfig {
             this.password = password;
         }
 
-        public String getOptionString() {
-            return optionString;
+    }
+
+    /**
+     * Dump的映射关系。
+     */
+    public static class DumpRelation {
+        private String srcDatabaseName;
+        private String destDatabaseName;
+        private List<String> srcTableNames;
+        private List<String> destTableNames;
+
+        public String getSrcDatabaseName() {
+            return srcDatabaseName;
         }
 
-        public void setOptionString(String optionString) {
-            this.optionString = optionString;
+        public void setSrcDatabaseName(String srcDatabaseName) {
+            this.srcDatabaseName = srcDatabaseName;
+        }
+
+        public String getDestDatabaseName() {
+            return destDatabaseName;
+        }
+
+        public void setDestDatabaseName(String destDatabaseName) {
+            this.destDatabaseName = destDatabaseName;
+        }
+
+        public List<String> getSrcTableNames() {
+            return srcTableNames;
+        }
+
+        public void setSrcTableNames(List<String> srcTableNames) {
+            this.srcTableNames = srcTableNames;
+        }
+
+        public List<String> getDestTableNames() {
+            return destTableNames;
+        }
+
+        public void setDestTableNames(List<String> destTableNames) {
+            this.destTableNames = destTableNames;
         }
 
     }
-
 }
--- puma/puma-syncserver/src/main/java/com/dianping/puma/syncserver/bo/DumpClient.java
+++ puma/puma-syncserver/src/main/java/com/dianping/puma/syncserver/bo/DumpClient.java
@@ -3,11 +3,11 @@ package com.dianping.puma.syncserver.bo;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,100 +27,95 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.dianping.puma.core.sync.DumpConfig;
+import com.dianping.puma.core.sync.DumpConfig.Dest;
+import com.dianping.puma.core.sync.DumpConfig.DumpRelation;
 import com.dianping.puma.core.sync.DumpConfig.Src;
 
 /**
- * dump命令需要注意的地方：<br>
- * (1)LOCK TABLES if the --single-transaction option is not used. <br>
- * dump： mysqldump --host=127.0.0.1 --user=root --password=root --databases pumatest --opt --add-drop-database=false
- * --add-drop-table=false --default-character-set=utf8 --result-file=file_name
- * 
  * @author wukezhu
  */
-//dump：mysqldump --host=127.0.0.1 --user=root --password=root --opt --add-drop-database=false --add-drop-table=false --default-character-set=utf8 --result-file=file_name <database_name> <table_name> <table_name>
-//load：
 public class DumpClient {
     private static final Logger LOG = LoggerFactory.getLogger(DumpClient.class);
-
-    private final String uuid;
-    private final String outputDir;
-    //-- CHANGE MASTER TO MASTER_LOG_FILE='mysql-bin.000056', MASTER_LOG_POS=107;
     private final static Pattern BINLOG_LINE_PATTERN = Pattern.compile("^.+LOG_FILE='(.*)',\\s+.+LOG_POS=([0-9]+);$");
 
+    private static final String BASE_DIR = "/data/appdatas/puma/syncserver/";
+    private static final String SHELL_DIR = BASE_DIR + "shell/";
+
+    private final String uuid;
+    private final String dumpOutputDir;
+
+    private Executor executor = new DefaultExecutor();
+    {
+        executor.setExitValue(1);
+        ExecuteWatchdog watchdog = new ExecuteWatchdog(60 * 1000);
+        executor.setWatchdog(watchdog);
+    }
     private DumpConfig dumpConfig;
+
+    private Process proc;
 
     public DumpClient(DumpConfig dumpConfig) {
         this.uuid = UUID.randomUUID().toString();
-        this.outputDir = "/data/appdatas/puma-syncserver/" + uuid + "/";
-        new File(outputDir).mkdir();
+        this.dumpOutputDir = BASE_DIR + "dump/" + uuid + "/";
+        new File(dumpOutputDir).mkdir();
         this.dumpConfig = dumpConfig;
     }
 
-    //如果一次mysqldump只能指定一个database下的若干table，那么多个database时，得分多次mysqldump。三种方法:
-    //（1）为保证不同database的mysqldump的state(binlog)一致，需要手动lock <指定table> read；多次dump完，再unlock read。
-    //      但是手动lock read后，dump里自动会无法lock read！！ (好像dump一定会自动 lock read，有没有办法skip？)
-    //（2）允许不同database的mysqldump的state(binlog)不一致，这样需要为不同database做dump和PumaClient的追赶  X
-    //（3）修改sync.xml时，一次只允许新增/修改一个<database>/<table>，这样mysqldump只处理一个database，所以不存在不一致
     /**
-     * 根据dumpConfig，进行dump，并返回binlog位置
-     * 
-     * @throws IOException
-     * @throws ExecuteException
-     * @throws InterruptedException
+     * 根据dumpConfig，进行dump，并返回binlog位置<br>
+     * <br>
+     * 如果一次mysqldump只能指定一个database下的若干table，那么多个database时，得分多次mysqldump。<br>
+     * 解决方法:<br>
+     * 允许不同database的mysqldump的state(binlog)不一致，这样需要为不同database做dump和PumaClient的追赶
      */
     public List<BinlogPos> dump() throws ExecuteException, IOException, InterruptedException {
         try {
             List<BinlogPos> binlogPosList = new ArrayList<BinlogPos>();
-
-            Executor executor = new DefaultExecutor();
-            executor.setExitValue(1);
-            ExecuteWatchdog watchdog = new ExecuteWatchdog(60 * 1000);
-            executor.setWatchdog(watchdog);
-
-            Map<String, List<String>> databaseName2TableNames = dumpConfig.getDatabaseName2TableNames();
-            for (Map.Entry<String, List<String>> entry : databaseName2TableNames.entrySet()) {
+            List<DumpRelation> dumpRelations = dumpConfig.getDumpRelations();
+            LOG.info("============ dump ===========");
+            for (DumpRelation dumpRelation : dumpRelations) {
                 //执行dump脚本，dump到<dump_tempDir>/<uuid>目录
-                DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
-                executor.setStreamHandler(streamHandler);
-                CommandLine cmdLine = new CommandLine("mysqldump");
-                cmdLine.addArgument("--host=" + dumpConfig.getSrc().getHost());
-                cmdLine.addArgument("--user=" + dumpConfig.getSrc().getUsername());
-                cmdLine.addArgument("--password=" + dumpConfig.getSrc().getPassword());
-                cmdLine.addArgument("--port=" + dumpConfig.getSrc().getPort());
-                for (String opt : dumpConfig.getSrc().getOptions()) {
-                    cmdLine.addArgument(opt);
-                }
-                String databaseName = entry.getKey();
-                String outputFile = getOutputFile(databaseName);
-                cmdLine.addArgument("--result-file=" + outputFile);
-                cmdLine.addArgument(databaseName);
-                List<String> tableNames = entry.getValue();
-                for (String tableName : tableNames) {
-                    cmdLine.addArgument(tableName);
-                }
-                LOG.info("start dumping...");
-                LOG.info("cmd is: " + cmdLine.toString());
-                executor.execute(cmdLine, resultHandler);
-                resultHandler.waitFor();
-                String output = outputStream.toString();
+                String srcDatabaseName = dumpRelation.getSrcDatabaseName();
+                List<String> srcTableNames = dumpRelation.getSrcTableNames();
+                String destDatabaseName = dumpRelation.getDestDatabaseName();
+                List<String> destTableNames = dumpRelation.getDestTableNames();
+                String output = _mysqldump(srcDatabaseName, srcTableNames);
                 if (StringUtils.isNotBlank(output)) {
                     throw new DumpException("mysqldump output is not empty , so consided to be failed: " + output);
                 }
                 LOG.info("dump done.");
                 BinlogPos binlogPos = new BinlogPos();
-                LineIterator lineIterators = IOUtils.lineIterator(new FileInputStream(outputFile), "UTF-8");
+                LineIterator lineIterators = IOUtils.lineIterator(new FileInputStream(_getDumpFile(srcDatabaseName)), "UTF-8");
+                PrintWriter deelFileWriter = new PrintWriter(new File(_getSourceFile(srcDatabaseName)), "UTF-8");
+                deelFileWriter.println("CREATE DATABASE IF NOT EXISTS " + destDatabaseName + ";USE " + destDatabaseName + ";");//添加select database语句
                 while (lineIterators.hasNext()) {
                     String line = lineIterators.next();
-                    //-- CHANGE MASTER TO MASTER_LOG_FILE='mysql-bin.000056', MASTER_LOG_POS=107;
-                    Matcher matcher = BINLOG_LINE_PATTERN.matcher(line);
-                    if (matcher.matches()) {
-                        binlogPos.setBinlogFile(matcher.group(1));
-                        binlogPos.setBinlogPosition(Long.parseLong(matcher.group(2)));
-                        break;
+                    //获取binlog位置
+                    if (StringUtils.isBlank(binlogPos.getBinlogFile()) || binlogPos.getBinlogPosition() == null) {
+                        Matcher matcher = BINLOG_LINE_PATTERN.matcher(line);
+                        if (matcher.matches()) {
+                            binlogPos.setBinlogFile(matcher.group(1));
+                            binlogPos.setBinlogPosition(Long.parseLong(matcher.group(2)));
+                        }
                     }
+                    //table更名
+                    for (int i = 0; i < srcTableNames.size(); i++) {
+                        String srcTableName = srcTableNames.get(i);
+                        String destTableName = destTableNames.get(i);
+                        String originLine = line;
+                        line = line.replace("INSERT INTO `" + srcTableName + "`", "INSERT INTO `" + destTableName + "`");
+                        line = line.replace("table `" + srcTableName + "`", "table `" + destTableName + "`");
+                        line = line.replace("TABLE `" + srcTableName + "`", "TABLE `" + destTableName + "`");
+                        if (!StringUtils.equals(line, originLine)) {//line已经改变，即table被替换过，则不需要再查找
+                            break;
+                        }
+                    }
+                    //替换 CREATE TABLE 为 CREATE TABLE IF NOT EXISTS
+                    line = line.replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS");
+
+                    deelFileWriter.println(line);
                 }
+                deelFileWriter.close();
                 if (StringUtils.isBlank(binlogPos.getBinlogFile()) || binlogPos.getBinlogPosition() == null) {
                     throw new DumpException("binlogFile or binlogPos is Error: binlogFile=" + binlogPos.getBinlogFile()
                             + ",binlogPos=" + binlogPos.getBinlogPosition());
@@ -129,44 +124,131 @@ public class DumpClient {
                 binlogPosList.add(binlogPos);
             }
             //load
-            
-
+            LOG.info("============ load ===========");
+            for (DumpRelation dumpRelation : dumpRelations) {
+                //执行dump脚本，dump到<dump_tempDir>/<uuid>目录
+                String srcDatabaseName = dumpRelation.getSrcDatabaseName();
+                String output = _mysqlload(srcDatabaseName);
+                if (StringUtils.isNotBlank(output)) {
+                    throw new DumpException("mysqlload output is not empty , so consided to be failed: " + output);
+                }
+                LOG.info("load done.");
+            }
             return binlogPosList;
         } catch (Exception e) {
             throw new DumpException("dump error!", e);
         }
     }
 
-    private String getOutputFile(String databaseName) {
-        return outputDir + databaseName + ".sql";
+    private String _getDumpFile(String databaseName) {
+        return dumpOutputDir + databaseName + ".dump.sql";
+    }
+
+    private String _getSourceFile(String databaseName) {
+        return dumpOutputDir + databaseName + ".source.sql";
+    }
+
+    private String _mysqldump(String databaseName, List<String> tableNames) throws IOException, InterruptedException {
+        List<String> cmdlist = new ArrayList<String>();
+        cmdlist.add("mysqldump");
+        cmdlist.add("--host=" + dumpConfig.getSrc().getHost());
+        cmdlist.add("--user=" + dumpConfig.getSrc().getUsername());
+        cmdlist.add("--password=" + dumpConfig.getSrc().getPassword());
+        cmdlist.add("--port=" + dumpConfig.getSrc().getPort());
+        for (String opt : dumpConfig.getSrc().getOptions()) {
+            cmdlist.add(opt);
+        }
+        String outputFile = _getDumpFile(databaseName);
+        cmdlist.add("--result-file=" + outputFile);
+        cmdlist.add(databaseName);
+        for (String tableName : tableNames) {
+            cmdlist.add(tableName);
+        }
+        LOG.info("start dumping " + databaseName + " ...");
+        return _executeByApache(cmdlist.toArray(new String[0]));
+    }
+
+    private String _mysqlload(String databaseName) throws ExecuteException, IOException, InterruptedException {
+        List<String> cmdlist = new ArrayList<String>();
+        cmdlist.add(SHELL_DIR + "mysqlload.sh");
+        cmdlist.add("--host=" + dumpConfig.getDest().getHost());
+        cmdlist.add("--user=" + dumpConfig.getDest().getUsername());
+        cmdlist.add("--password=" + dumpConfig.getDest().getPassword());
+        cmdlist.add("--port=" + dumpConfig.getDest().getPort());
+        cmdlist.add(_getSourceFile(databaseName));
+        LOG.info("start loading " + databaseName + " ...");
+        return _executeByApache(cmdlist.toArray(new String[0]));
+    }
+
+    @SuppressWarnings("unused")
+    private String _execute(String[] cmdarray) throws IOException {
+        LOG.info("execute shell script, cmd is: " + Arrays.toString(cmdarray));
+        InputStream input = null;
+        try {
+            proc = Runtime.getRuntime().exec(cmdarray);
+            input = proc.getInputStream();
+            return IOUtils.toString(input);
+        } finally {
+            IOUtils.closeQuietly(input);
+        }
+    }
+
+    private String _executeByApache(String[] cmdarray) throws ExecuteException, IOException, InterruptedException {
+        DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
+        executor.setStreamHandler(streamHandler);
+        CommandLine cmdLine = new CommandLine(cmdarray[0]);
+        for (int i = 1; i < cmdarray.length; i++) {
+            cmdLine.addArgument(cmdarray[i]);
+        }
+        LOG.info("execute(by apache) shell script, cmd is: " + cmdLine.toString());
+        executor.execute(cmdLine, resultHandler);
+        resultHandler.waitFor();
+        return outputStream.toString();
     }
 
     public static void main(String[] args) throws ExecuteException, IOException, InterruptedException {
+        //mock dumpConfig
         DumpConfig dumpConfig = new DumpConfig();
-        Map<String, List<String>> databaseName2TableNames = new HashMap<String, List<String>>();
-        databaseName2TableNames.put("pumatest", Arrays.asList(new String[] { "test1", "test2" }));
-        databaseName2TableNames.put("test", Arrays.asList(new String[] { "test5" }));
-        dumpConfig.setDatabaseName2TableNames(databaseName2TableNames);
+        List<DumpRelation> dumpRelations = new ArrayList<DumpConfig.DumpRelation>();
+        DumpRelation r1 = new DumpConfig.DumpRelation();
+        //        r1.setSrcDatabaseName("pumatest");
+        //        r1.setSrcTableNames(Arrays.asList(new String[] { "test1", "test2" }));
+        //        r1.setDestDatabaseName("pumatest2");
+        //        r1.setDestTableNames(Arrays.asList(new String[] { "test11", "test22" }));
+        r1.setSrcDatabaseName("test");
+        r1.setSrcTableNames(Arrays.asList(new String[] { "test4", "test5" }));
+        r1.setDestDatabaseName("pumatest2");
+        r1.setDestTableNames(Arrays.asList(new String[] { "test7", "test8" }));
+        dumpRelations.add(r1);
+        DumpRelation r2 = new DumpConfig.DumpRelation();
+        r2.setSrcDatabaseName("pumatest");
+        r2.setSrcTableNames(Arrays.asList(new String[] { "test1", "test2" }));
+        r2.setDestDatabaseName("pumatest2");
+        r2.setDestTableNames(Arrays.asList(new String[] { "test12", "test13" }));
+        dumpRelations.add(r2);
+        dumpConfig.setDumpRelations(dumpRelations);
         Src src = new Src();
         src.setHost("127.0.0.1");
         src.setPassword("root");
         src.setPort(3306);
-        List<String> opts = Arrays.asList(new String[] { "--no-autocommit", " --disable-keys", "--extended-insert", "--quick",
+        List<String> opts = Arrays.asList(new String[] { "--no-autocommit", " --disable-keys", "--quick",
                 "--add-drop-database=false", "--add-drop-table=false", "--skip-add-locks", "--default-character-set=utf8",
-                "--max_allowed_packet=16777216", " --net_buffer_length=16384", "-i", "--master-data=2", "--single-transaction", });
+                "--max_allowed_packet=16777216", " --net_buffer_length=16384", "-i", "--master-data=2", "--single-transaction" });
         src.setOptions(opts);
         src.setUsername("root");
         dumpConfig.setSrc(src);
-
+        Dest dest = new Dest();
+        dest.setHost("192.168.7.43");
+        dest.setUsername("binlog");
+        dest.setPassword("binlog");
+        dest.setPort(3306);
+        dumpConfig.setDest(dest);
+        //调用dump
         DumpClient dumpClient = new DumpClient(dumpConfig);
-        dumpClient.dump();
-        
-      //-- CHANGE MASTER TO MASTER_LOG_FILE='mysql-bin.000056', MASTER_LOG_POS=107;
-//        Pattern pp = Pattern.compile("^.+LOG_FILE='(.*)',\\s+.+LOG_POS=([0-9]+);$");
-//        Matcher matcher = pp.matcher("-- CHANGE MASTER TO MASTER_LOG_FILE='mysql-bin.000056', MASTER_LOG_POS=107;");
-//        if (matcher.matches()) {
-//            System.out.println(matcher.group(1));
-//        }
+        System.out.println(dumpClient.dump());
+
     }
 
 }

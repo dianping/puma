fdc5c2c8f10ba1f42c20dc5dccdb1af5fcf55c1b
xiaotian.li <xiaotian.li@dianping.com>
2015-03-03 18:39:14 +0800

    split puma task operation and controller checker

---
+++ puma-server/src/main/java/com/dianping/puma/checker/PumaTaskControllerChecker.java
@@ -0,0 +1,49 @@
+package com.dianping.puma.checker;
+
+import com.dianping.puma.core.constant.Controller;
+import com.dianping.puma.core.model.PumaTaskController;
+import com.dianping.puma.core.monitor.Event;
+import com.dianping.puma.core.monitor.EventListener;
+import com.dianping.puma.core.monitor.PumaTaskControllerEvent;
+import com.dianping.puma.server.TaskExecutorContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("pumaTaskControllerChecker")
+public class PumaTaskControllerChecker implements EventListener {
+
+	public static final Logger LOG = LoggerFactory.getLogger(PumaTaskControllerChecker.class);
+
+	@Autowired
+	private TaskExecutorContainer taskExecutorContainer;
+
+	@Override
+	public void onEvent(Event event) {
+		LOG.info("Receive puma task event!");
+
+		if (event instanceof PumaTaskControllerEvent) {
+			LOG.info("Receive puma task controller event.");
+
+			PumaTaskControllerEvent pumaTaskControllerEvent = (PumaTaskControllerEvent) event;
+			PumaTaskController pumaTaskController = pumaTaskControllerEvent.getController();
+			Controller controller = pumaTaskController.getController();
+
+			switch (controller) {
+			case PAUSE:
+				LOG.info("Receive puma task controller event: PAUSE.");
+				taskExecutorContainer.pauseEvent(pumaTaskControllerEvent);
+				break;
+			case RESUME:
+				LOG.info("Receive puma task controller event: RESUME.");
+				taskExecutorContainer.resumeEvent(pumaTaskControllerEvent);
+				break;
+			default:
+				LOG.error("Receive illegal puma task controller event `{}`.", controller);
+			}
+		} else {
+			LOG.error("Receive illegal puma task event `{}`.", event);
+		}
+	}
+}

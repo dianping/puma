541259f12830d5cd982798666393955c93aeef96
xiaotian.li <xiaotian.li@dianping.com>
2015-03-25 15:31:25 +0800

    remove deprecated code

--- puma-syncserver/src/main/java/com/dianping/puma/syncserver/service/impl/TaskServiceImpl.java
+++ puma-syncserver/src/main/java/com/dianping/puma/syncserver/service/impl/TaskServiceImpl.java
@@ -3,12 +3,8 @@ package com.dianping.puma.syncserver.service.impl;
 import com.dianping.puma.core.entity.AbstractBaseSyncTask;
 import com.dianping.puma.core.model.BinlogInfo;
 import com.dianping.puma.syncserver.service.BinlogInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.dianping.puma.core.sync.dao.task.CatchupTaskDao;
-import com.dianping.puma.core.sync.dao.task.DumpTaskDao;
-import com.dianping.puma.core.sync.dao.task.SyncTaskDao;
 import com.dianping.puma.syncserver.service.TaskService;
 
 @Service

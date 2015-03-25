e6192e21f4279afdbf5692770f8aa43de1ef6a03
xiaotian.li <xiaotian.li@dianping.com>
2015-03-25 14:16:57 +0800

    add sync controller

--- puma-syncserver/src/main/java/com/dianping/puma/syncserver/remote/receiver/SyncTaskControllerReceiver.java
+++ puma-syncserver/src/main/java/com/dianping/puma/syncserver/remote/receiver/SyncTaskControllerReceiver.java
@@ -3,8 +3,10 @@ package com.dianping.puma.syncserver.remote.receiver;
 import com.dianping.puma.core.monitor.event.Event;
 import com.dianping.puma.core.monitor.EventListener;
 import com.dianping.puma.core.monitor.event.SyncTaskControllerEvent;
+import com.dianping.puma.syncserver.job.container.TaskExecutorContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("syncTaskControllerReceiver")
@@ -12,17 +14,17 @@ public class SyncTaskControllerReceiver implements EventListener {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SyncTaskControllerEvent.class);
 
+	@Autowired
+	TaskExecutorContainer taskExecutorContainer;
+
 	@Override
 	public void onEvent(Event event) {
 		if (event instanceof SyncTaskControllerEvent) {
 			LOG.info("Receive sync task controller event.");
 
-
-		} else {
-			LOG.warn("Receive illegal sync task controller event: {}.", event);
-
-			// @TODO
-			// Log event exception into CAT.
+			SyncTaskControllerEvent syncTaskControllerEvent = (SyncTaskControllerEvent) event;
+			taskExecutorContainer
+					.changeStatus(syncTaskControllerEvent.getTaskName(), syncTaskControllerEvent.getController());
 		}
 	}
 }

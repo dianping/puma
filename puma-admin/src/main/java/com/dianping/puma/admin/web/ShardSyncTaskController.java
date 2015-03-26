7698fced92006b8d5046c45f1633ead5b088ec31
Dozer <mail@dozer.cc>
2015-03-26 17:27:16 +0800

    add shard sync task controller

---
+++ puma-admin/src/main/java/com/dianping/puma/admin/web/ShardSyncTaskController.java
@@ -0,0 +1,19 @@
+package com.dianping.puma.admin.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Dozer @ 2015-02
+ * mail@dozer.cc
+ * http://www.dozer.cc
+ */
+
+@Controller
+public class ShardSyncTaskController {
+    private static final Logger LOG = LoggerFactory.getLogger(ShardSyncTaskController.class);
+
+
+
+}

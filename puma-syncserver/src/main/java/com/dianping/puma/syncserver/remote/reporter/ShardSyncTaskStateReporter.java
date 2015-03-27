e836dbfafd96b422ae368ab81d8de79092d3429a
Dozer <mail@dozer.cc>
2015-03-27 15:23:41 +0800

    fix bean name

--- puma-syncserver/src/main/java/com/dianping/puma/syncserver/remote/reporter/ShardSyncTaskStateReporter.java
+++ puma-syncserver/src/main/java/com/dianping/puma/syncserver/remote/reporter/ShardSyncTaskStateReporter.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Service("syncTaskStateReporter")
+@Service("shardSyncTaskStateReporter")
 public class ShardSyncTaskStateReporter {
 
     @Autowired

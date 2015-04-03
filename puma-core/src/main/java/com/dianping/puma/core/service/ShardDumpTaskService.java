0430ebf59bd73d63b5a8b07dbbc97cacc19f7493
Dozer <mail@dozer.cc>
2015-04-03 11:09:47 +0800

    add service

---
+++ puma-core/src/main/java/com/dianping/puma/core/service/ShardDumpTaskService.java
@@ -0,0 +1,20 @@
+package com.dianping.puma.core.service;
+
+import com.dianping.puma.core.entity.ShardDumpTask;
+
+import java.util.List;
+
+public interface ShardDumpTaskService {
+
+    ShardDumpTask find(String name);
+
+    List<ShardDumpTask> findBySyncServerName(String syncServerName);
+
+    List<ShardDumpTask> findAll();
+
+    void create(ShardDumpTask syncTask);
+
+    void remove(String name);
+
+    List<ShardDumpTask> find(int offset, int limit);
+}
---
+++ puma-core/src/main/java/com/dianping/puma/core/service/impl/ShardDumpTaskServiceImpl.java
@@ -0,0 +1,40 @@
+package com.dianping.puma.core.service.impl;
+
+import com.dianping.puma.core.dao.ShardDumpTaskDao;
+import com.dianping.puma.core.entity.ShardDumpTask;
+import com.dianping.puma.core.service.ShardDumpTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("shardDumpTaskService")
+public class ShardDumpTaskServiceImpl implements ShardDumpTaskService {
+
+    @Autowired
+    ShardDumpTaskDao shardDumpTaskDao;
+
+    public ShardDumpTask find(String name) {
+        return shardDumpTaskDao.find(name);
+    }
+
+    public List<ShardDumpTask> findBySyncServerName(String syncServerName) {
+        return shardDumpTaskDao.findBySyncServerName(syncServerName);
+    }
+
+    public List<ShardDumpTask> findAll() {
+        return shardDumpTaskDao.findAll();
+    }
+
+    public void create(ShardDumpTask syncTask) {
+        shardDumpTaskDao.create(syncTask);
+    }
+
+    public void remove(String name) {
+        shardDumpTaskDao.remove(name);
+    }
+
+    public List<ShardDumpTask> find(int offset, int limit) {
+        return shardDumpTaskDao.find(offset, limit);
+    }
+}

b5fa95fba4e3fa6be79c6d568480c01605a6f44d
xiaotian.li <xiaotian.li@dianping.com>
2015-04-27 15:08:58 +0800

    add dbtbfilter name

--- puma-server/src/main/java/com/dianping/puma/server/builder/impl/DefaultTaskExecutorBuilder.java
+++ puma-server/src/main/java/com/dianping/puma/server/builder/impl/DefaultTaskExecutorBuilder.java
@@ -161,6 +161,7 @@ public class DefaultTaskExecutorBuilder implements TaskExecutorBuilder {
 			// Schema table filter.
 			DbTbEventFilter dbtbFilter = new DbTbEventFilter();
 			dbtbFilter.init(new String[]{});
+			dbtbFilter.setName(taskName);
 			eventCenter.register(dbtbFilter);
 			eventFilterList.add(dbtbFilter);
 

86e97db29a7e8402aa1647fb0d8ded2bdd6b4661
xiaotian.li <xiaotian.li@dianping.com>
2015-04-28 11:05:13 +0800

    add schema and schema set data structure

--- puma-core/src/main/java/com/dianping/puma/core/model/Schema.java
+++ puma-core/src/main/java/com/dianping/puma/core/model/Schema.java
@@ -2,13 +2,54 @@ package com.dianping.puma.core.model;
 
 public class Schema {
 
-	private String schema;
+	private String schemaName;
+
+	private static final String ASTERISK = "*";
 
 	public Schema() {}
 
-	public Schema(String schema) {
-		this.schema = schema;
+	public Schema(String schemaName) {
+		this.schemaName = schemaName;
 	}
 
+	public boolean contains(Schema schema) {
+		if (schema != null) {
+			return this.schemaName.equals(ASTERISK) || this.schemaName.equals(schema.schemaName);
+		}
+		return false;
+	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		Schema schema = (Schema) o;
+
+		if (!schemaName.equals(schema.schemaName))
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return schemaName.hashCode();
+	}
+
+	@Override public String toString() {
+		return "Schema{" +
+				"schemaName='" + schemaName + '\'' +
+				'}';
+	}
+
+	public String getSchemaName() {
+		return schemaName;
+	}
+
+	public void setSchemaName(String schemaName) {
+		this.schemaName = schemaName;
+	}
 }

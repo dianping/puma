43241209282f9b27be5c4cea4a0ac60066b591d7
qi.yin <qi.yin@dianping.com>
2015-05-14 09:47:50 +0800

    BinlogInfo add toString() function.

--- puma-core/src/main/java/com/dianping/puma/core/model/BinlogInfo.java
+++ puma-core/src/main/java/com/dianping/puma/core/model/BinlogInfo.java
@@ -2,42 +2,48 @@ package com.dianping.puma.core.model;
 
 public class BinlogInfo {
 
-    private String binlogFile;
+	private String binlogFile;
 
-    private Long binlogPosition;
+	private Long binlogPosition;
 
-    private Boolean skipToNextPos = false;
+	private Boolean skipToNextPos = false;
 
-    public BinlogInfo() {}
+	public BinlogInfo() {
+	}
 
-    public BinlogInfo(String binlogFile, Long binlogPosition) {
-        this.binlogFile = binlogFile;
-        this.binlogPosition = binlogPosition;
-    }
+	public BinlogInfo(String binlogFile, Long binlogPosition) {
+		this.binlogFile = binlogFile;
+		this.binlogPosition = binlogPosition;
+	}
 
-    public String getBinlogFile() {
-        return binlogFile;
-    }
+	public String getBinlogFile() {
+		return binlogFile;
+	}
 
-    public void setBinlogFile(String binlogFile) {
-        this.binlogFile = binlogFile;
-    }
+	public void setBinlogFile(String binlogFile) {
+		this.binlogFile = binlogFile;
+	}
 
-    public Long getBinlogPosition() {
-        return binlogPosition;
-    }
+	public Long getBinlogPosition() {
+		return binlogPosition;
+	}
 
-    public void setBinlogPosition(Long binlogPosition) {
-        this.binlogPosition = binlogPosition;
-    }
+	public void setBinlogPosition(Long binlogPosition) {
+		this.binlogPosition = binlogPosition;
+	}
 
-    public Boolean isSkipToNextPos() {
-        return skipToNextPos;
-    }
+	public Boolean isSkipToNextPos() {
+		return skipToNextPos;
+	}
 
-    public void setSkipToNextPos(Boolean skipToNextPos) {
-        this.skipToNextPos = skipToNextPos;
-    }
+	public void setSkipToNextPos(Boolean skipToNextPos) {
+		this.skipToNextPos = skipToNextPos;
+	}
 
-	
+	@Override
+	public String toString() {
+		return "BinlogInfo [binlogFile=" + binlogFile + ", binlogPosition=" + binlogPosition + ",skipToNextPos="
+				+ skipToNextPos + " ]";
+	}
+
 }

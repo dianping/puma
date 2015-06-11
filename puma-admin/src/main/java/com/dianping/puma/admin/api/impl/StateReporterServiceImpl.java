05c576d918c144b320f3490252d57aeff2dea919
qi.yin <qi.yin@dianping.com>
2015-06-11 11:38:45 +0800

    update StateReporterServiceImpl.

--- puma-admin/src/main/java/com/dianping/puma/admin/api/impl/StateReporterServiceImpl.java
+++ puma-admin/src/main/java/com/dianping/puma/admin/api/impl/StateReporterServiceImpl.java
@@ -3,11 +3,13 @@ package com.dianping.puma.admin.api.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.dianping.pigeon.remoting.provider.config.annotation.Service;
 import com.dianping.puma.core.api.StateReporterService;
 import com.dianping.puma.core.model.RelatedInfo;
 import com.dianping.puma.core.model.ClientAck;
 import com.dianping.puma.core.model.ServerAck;
 
+@Service
 public class StateReporterServiceImpl implements StateReporterService {
 	private static final Logger LOG = LoggerFactory.getLogger(StateReporterServiceImpl.class);
 

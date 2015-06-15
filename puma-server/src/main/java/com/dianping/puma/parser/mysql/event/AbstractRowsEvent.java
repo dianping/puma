4d39e51342f8b22b2d2c2b1284996ab60e0c6633
xiaotian.li <xiaotian.li@dianping.com>
2015-06-15 17:24:17 +0800

    fix bug

--- puma-server/src/main/java/com/dianping/puma/parser/mysql/event/AbstractRowsEvent.java
+++ puma-server/src/main/java/com/dianping/puma/parser/mysql/event/AbstractRowsEvent.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 
+import com.dianping.cat.Cat;
 import org.apache.commons.lang.exception.NestableRuntimeException;
 
 import com.dianping.puma.bo.PumaContext;

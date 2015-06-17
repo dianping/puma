177db129286e51cc45fec35ddf5f7f05d7d3475b
qi.yin <qi.yin@dianping.com>
2015-06-17 11:18:23 +0800

    add cache write test.

---
+++ puma-admin/src/test/java/com/dianping/puma/admin/cache/CacheServiceMainTest.java
@@ -0,0 +1,28 @@
+package com.dianping.puma.admin.cache;
+
+import java.util.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dianping.puma.admin.common.StateContainer;
+import com.dianping.puma.core.model.BinlogInfo;
+import com.dianping.puma.core.model.ClientAck;
+
+public class CacheServiceMainTest {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		StateContainer stateContainer = (StateContainer) applicationContext.getBean("stateContainer");
+		ClientAck clientAck = new ClientAck();
+		clientAck.setClientName("lixt");
+		clientAck.setCreateDate(new Date());
+		BinlogInfo binlogInfo = new BinlogInfo();
+		binlogInfo.setBinlogFile("mysql-bin.000832");
+		binlogInfo.setBinlogPosition(123142170L);
+		binlogInfo.setEventIndex(0);
+		clientAck.setBinlogInfo(binlogInfo);
+		stateContainer.setClientAckInfo(clientAck);
+	}
+
+}

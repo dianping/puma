f560e6fc1a8244b3af77eec4a5a12836f90b6e6f
qi.yin <qi.yin@dianping.com>
2015-06-19 10:49:09 +0800

    StateFetcher record log.

--- puma-admin/src/main/java/com/dianping/puma/admin/api/impl/StateFetcherServiceImpl.java
+++ puma-admin/src/main/java/com/dianping/puma/admin/api/impl/StateFetcherServiceImpl.java
@@ -3,6 +3,8 @@ package com.dianping.puma.admin.api.impl;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,16 +17,26 @@ import com.dianping.puma.core.model.ClientRelatedInfo;
 @Component
 public class StateFetcherServiceImpl implements StateFetcherService {
 
+	private static final Logger LOG = LoggerFactory.getLogger(StateFetcherServiceImpl.class);
+
 	@Autowired
 	private StateCacheService stateCacheService;
-	
+
 	@Override
 	public ClientRelatedInfo getRelatedInfo(String clientName) {
+		if (clientName == null) {
+			return null;
+		}
+		LOG.info("Client get ClientName: {} related info.", clientName);
 		return stateCacheService.getClientRelatedInfo(clientName);
 	}
 
 	@Override
 	public Map<String, ClientRelatedInfo> getRelatedInfos(List<String> clientNames) {
+		if (clientNames == null && clientNames.size() == 0) {
+			return null;
+		}
+		LOG.info("Client get related infos.");
 		return stateCacheService.getClientRelatedInfos(clientNames);
 	}
 

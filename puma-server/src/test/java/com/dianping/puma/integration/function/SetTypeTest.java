0fe83978369e3481cada3ecd32c107719f1d1fab
qi.yin <qi.yin@dianping.com>
2015-06-25 19:00:33 +0800

    bit,enum,set type test.

--- puma-server/src/test/java/com/dianping/puma/integration/function/SetTypeTest.java
+++ puma-server/src/test/java/com/dianping/puma/integration/function/SetTypeTest.java
@@ -1,5 +1,65 @@
 package com.dianping.puma.integration.function;
 
-public class SetTypeTest extends AbstractBaseTest {
+import java.util.List;
 
+import junit.framework.Assert;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dianping.puma.core.event.ChangedEvent;
+import com.dianping.puma.core.event.RowChangedEvent;
+
+public class SetTypeTest extends AbstractBaseTest {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(SetTypeTest.class);
+	
+	private static final String TABLE_NAME = "tb_set";
+	
+	@BeforeClass
+	public static void doBefore() throws Exception {
+		String create_SQL = "CREATE TABLE IF NOT EXISTS `" + SCHEMA_NAME +"`.`" + TABLE_NAME + "` (\n"
+				+ "`id` int NOT NULL AUTO_INCREMENT, \n" + "`default_set` set('x','y','z') DEFAULT NULL, \n"
+				+ "PRIMARY KEY (`id`)"
+				+ ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+		queryRunner.update(create_SQL);
+		setFilterTable(TABLE_NAME);
+	}
+
+	@AfterClass
+	public static void doAfter() throws Exception {
+		String drop_SQL = "DROP TABLE IF EXISTS `" + SCHEMA_NAME +"`.`" + TABLE_NAME + "`";
+		queryRunner.update(drop_SQL);
+	}
+
+	@Test
+	public void setTypeInsertTest() throws Exception {
+		test(new TestLogic() {
+
+			@Override
+			public void doLogic() throws Exception {
+				String [][] testData = {{"x"},{"y"},{"z"}};
+				for(int i = 0; i < testData.length; i++){
+					String insert_SQL = "INSERT INTO `" + SCHEMA_NAME +"`.`" + TABLE_NAME + "`(default_set)VALUES(?)";
+					queryRunner.update(insert_SQL, testData[i][0]);
+				}
+				List<ChangedEvent> events = getEvents(testData.length, false, true, false);
+				Assert.assertEquals(testData.length, events.size());
+				for(int i = 0; i < testData.length; i++){
+					Assert.assertTrue(events.get(i) instanceof RowChangedEvent);
+					RowChangedEvent rowChangedEvent = (RowChangedEvent) events.get(i);
+					Assert.assertEquals(RowChangedEvent.INSERT, rowChangedEvent.getActionType());
+					Assert.assertEquals(TABLE_NAME, rowChangedEvent.getTable());
+					Assert.assertEquals(SCHEMA_NAME, rowChangedEvent.getDatabase());
+					Assert.assertEquals(2, rowChangedEvent.getColumns().size());
+					Assert.assertEquals(String.valueOf((int)Math.pow(2, i)), String.valueOf(rowChangedEvent.getColumns().get("default_set").getNewValue()));
+					Assert.assertEquals(null, rowChangedEvent.getColumns().get("default_set").getOldValue());
+				}
+			}
+
+		});	
+	}
 }

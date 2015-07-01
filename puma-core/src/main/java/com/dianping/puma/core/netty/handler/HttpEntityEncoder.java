9f1d69f0ab8b8d31db55b4b1ae707f2a9193a64a
Dozer <mail@dozer.cc>
2015-07-01 17:48:56 +0800

    do not encode HttpResponse entity

--- puma-core/src/main/java/com/dianping/puma/core/netty/handler/HttpEntityEncoder.java
+++ puma-core/src/main/java/com/dianping/puma/core/netty/handler/HttpEntityEncoder.java
@@ -4,10 +4,8 @@ import com.dianping.puma.core.util.ConvertHelper;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.*;
+import io.netty.util.ReferenceCounted;
 
 import java.util.List;
 
@@ -23,11 +21,19 @@ public class HttpEntityEncoder extends MessageToMessageEncoder<Object> {
 
     @Override
     protected void encode(ChannelHandlerContext ctx, Object msg, List<Object> out) throws Exception {
-        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-        response.headers().add(HttpHeaders.Names.CONTENT_TYPE, "application/json");
-        byte[] data = ConvertHelper.toBytes(msg);
-        response.content().writeBytes(data);
-        response.headers().add(HttpHeaders.Names.CONTENT_LENGTH, data.length);
-        out.add(response);
+        if (msg instanceof HttpResponse) {
+            HttpResponse response = (HttpResponse) msg;
+            if (response instanceof ReferenceCounted) {
+                ((ReferenceCounted) response).retain();
+                out.add(response);
+            }
+        } else {
+            DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+            response.headers().add(HttpHeaders.Names.CONTENT_TYPE, "application/json");
+            byte[] data = ConvertHelper.toBytes(msg);
+            response.content().writeBytes(data);
+            response.headers().add(HttpHeaders.Names.CONTENT_LENGTH, data.length);
+            out.add(response);
+        }
     }
 }

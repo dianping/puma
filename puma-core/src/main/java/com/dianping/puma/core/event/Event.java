27facaa1a541caa4c8528aa70e8fc4cc48311c77
xiaotian.li <xiaotian.li@dianping.com>
2015-07-01 18:38:53 +0800

    add binlog message skeleton

--- puma-core/src/main/java/com/dianping/puma/core/event/Event.java
+++ puma-core/src/main/java/com/dianping/puma/core/event/Event.java
@@ -13,5 +13,4 @@ public abstract class Event implements Serializable {
 	public long getSeq() {
 		return seq;
 	}
-
 }
--- puma-core/src/main/java/com/dianping/puma/core/netty/entity/BinlogMessage.java
+++ puma-core/src/main/java/com/dianping/puma/core/netty/entity/BinlogMessage.java
@@ -1,4 +1,32 @@
 package com.dianping.puma.core.netty.entity;
 
+import com.dianping.puma.core.event.ChangedEvent;
+import com.dianping.puma.core.model.BinlogInfo;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class BinlogMessage {
+
+	private List<ChangedEvent> binlogEvents = new LinkedList<ChangedEvent>();
+
+	public List<ChangedEvent> getBinlogEvents() {
+		return binlogEvents;
+	}
+
+	public void setBinlogEvents(List<ChangedEvent> binlogEvents) {
+		this.binlogEvents = binlogEvents;
+	}
+
+	public void addBinlogEvents(ChangedEvent binlogEvent) {
+		binlogEvents.add(binlogEvent);
+	}
+
+	public BinlogInfo getLastBinlogInfo() {
+		return null;
+	}
+
+	public int size() {
+		return binlogEvents.size();
+	}
 }

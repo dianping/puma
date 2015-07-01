b0c44b4501a79aa53e3b3b37bd818562852b5fc9
xiaotian.li <xiaotian.li@dianping.com>
2015-07-01 18:37:23 +0800

    add pumaserver binlog query decoder

--- puma-server/src/main/java/com/dianping/puma/pumaserver/router/decoder/BinlogQueryDecoder.java
+++ puma-server/src/main/java/com/dianping/puma/pumaserver/router/decoder/BinlogQueryDecoder.java
@@ -2,12 +2,21 @@ package com.dianping.puma.pumaserver.router.decoder;
 
 import com.dianping.puma.core.netty.entity.BinlogQuery;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.QueryStringDecoder;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 public class BinlogQueryDecoder implements RequestDecoder {
 
-	Pattern pattern = Pattern.compile("^/puma/binlog/query/*$");
+	private static final boolean   DEFAULT_AUTO_ACK    = false;
+	private static final int       DEFAULT_BATCH_SIZE  = 1;
+	private static final long      DEFAULT_TIMEOUT     = 0;
+	private static final TimeUnit  DEFAULT_TIME_UNIT   = TimeUnit.MILLISECONDS;
+
+	Pattern pattern = Pattern.compile("^/puma/binlog/query.*$");
 
 	@Override
 	public boolean match(FullHttpRequest request) {
@@ -16,7 +25,25 @@ public class BinlogQueryDecoder implements RequestDecoder {
 
 	@Override
 	public Object decode(FullHttpRequest request) {
+		Map<String, List<String>> params = (new QueryStringDecoder(request.getUri())).parameters();
 		BinlogQuery binlogQuery = new BinlogQuery();
+
+		binlogQuery.setAutoAck(
+				params.containsKey("autoAck") ? Boolean.valueOf(params.get("autoAck").get(0)) : DEFAULT_AUTO_ACK
+		);
+
+		binlogQuery.setBatchSize(
+				params.containsKey("batchSize") ? Integer.valueOf(params.get("batchSize").get(0)) : DEFAULT_BATCH_SIZE
+		);
+
+		binlogQuery.setTimeout(
+				params.containsKey("timeout") ? Long.valueOf(params.get("timeout").get(0)) : DEFAULT_TIMEOUT
+		);
+
+		binlogQuery.setTimeUnit(
+				params.containsKey("timeUnit") ? TimeUnit.valueOf(params.get("timeUnit").get(0)) : DEFAULT_TIME_UNIT
+		);
+
 		return binlogQuery;
 	}
 }

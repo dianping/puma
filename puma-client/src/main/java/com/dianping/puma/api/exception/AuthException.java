cc7ebbc6e09416c544f863b7521edf40f92a9e18
Dozer <mail@dozer.cc>
2015-07-07 10:53:45 +0800

    remove old puma client

--- puma-client/src/main/java/com/dianping/puma/api/exception/AuthException.java
+++
@@ -1,8 +0,0 @@
-package com.dianping.puma.api.exception;
-
-public class AuthException extends RuntimeException {
-
-	public AuthException(String msg) {
-		super(msg);
-	}
-}
--- puma-client/src/main/java/com/dianping/puma/api/util/Clock.java
+++
@@ -1,8 +0,0 @@
-package com.dianping.puma.api.util;
-
-public class Clock {
-
-	public long getCurrentTime() {
-		return System.currentTimeMillis();
-	}
-}

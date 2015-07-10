24f45ba72c4c929ff5387d75c85dede001432d05
Dozer <mail@dozer.cc>
2015-07-10 18:27:51 +0800

    add fields

--- puma-biz/src/main/java/com/dianping/puma/biz/entity/PumaTaskEntity.java
+++ puma-biz/src/main/java/com/dianping/puma/biz/entity/PumaTaskEntity.java
@@ -2,18 +2,26 @@ package com.dianping.puma.biz.entity;
 
 import com.dianping.puma.core.constant.Status;
 import com.dianping.puma.core.model.BinlogInfo;
+import com.dianping.puma.core.model.TableSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PumaTaskEntity {
 
-	private int id;
+    private int id;
 
-	private String name;
+    private String name;
 
-	private BinlogInfo binlogInfo;
+    private BinlogInfo binlogInfo;
 
-	private int preservedDay;
+    private int preservedDay;
 
     private Status status;
+
+    private List<PumaTaskDbEntity> pumaTaskDbEntities = new ArrayList<PumaTaskDbEntity>();
+
+    private TableSet tableSet = new TableSet();
 
     public Status getStatus() {
         return status;
@@ -24,34 +32,42 @@ public class PumaTaskEntity {
     }
 
     public int getId() {
-		return id;
-	}
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public int getPreservedDay() {
-		return preservedDay;
-	}
+    public int getPreservedDay() {
+        return preservedDay;
+    }
 
-	public void setPreservedDay(int preservedDay) {
-		this.preservedDay = preservedDay;
-	}
+    public void setPreservedDay(int preservedDay) {
+        this.preservedDay = preservedDay;
+    }
 
-	public BinlogInfo getBinlogInfo() {
-		return binlogInfo;
-	}
+    public BinlogInfo getBinlogInfo() {
+        return binlogInfo;
+    }
 
-	public void setBinlogInfo(BinlogInfo binlogInfo) {
-		this.binlogInfo = binlogInfo;
-	}
+    public void setBinlogInfo(BinlogInfo binlogInfo) {
+        this.binlogInfo = binlogInfo;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PumaTaskDbEntity> getPumaTaskDbEntities() {
+        return pumaTaskDbEntities;
+    }
+
+    public TableSet getTableSet() {
+        return tableSet;
+    }
 }

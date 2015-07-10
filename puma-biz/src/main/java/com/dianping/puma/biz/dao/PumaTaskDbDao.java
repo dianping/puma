ec6cac3efe2ccba45d6cf42743d8edfea26694be
Dozer <mail@dozer.cc>
2015-07-10 14:16:16 +0800

    add puma task db dao

---
+++ puma-biz/src/main/java/com/dianping/puma/biz/dao/PumaTaskDbDao.java
@@ -0,0 +1,22 @@
+package com.dianping.puma.biz.dao;
+
+import com.dianping.puma.biz.entity.PumaTaskDbEntity;
+import com.dianping.puma.biz.entity.SrcDbEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Dozer @ 7/10/15
+ * mail@dozer.cc
+ * http://www.dozer.cc
+ */
+public interface PumaTaskDbDao {
+
+    List<PumaTaskDbEntity> findByTaskId(@Param(value = "taskId") int taskId);
+
+    int delete(@Param(value = "id") int id);
+
+    int insert(SrcDbEntity entity);
+
+}

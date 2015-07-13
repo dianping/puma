f013098865c53270a93870bd0d0f17aa783d6516
xiaotian.li <xiaotian.li@dianping.com>
2015-07-13 13:52:23 +0800

    add puma server task checker skeleton

---
+++ puma-server/src/main/java/com/dianping/puma/server/checker/AbstractTaskChecker.java
@@ -0,0 +1,6 @@
+package com.dianping.puma.server.checker;
+
+import com.dianping.puma.common.AbstractLifeCycle;
+
+public abstract class AbstractTaskChecker extends AbstractLifeCycle implements TaskChecker {
+}
---
+++ puma-server/src/main/java/com/dianping/puma/server/checker/PolledTaskChecker.java
@@ -0,0 +1,22 @@
+package com.dianping.puma.server.checker;
+
+import com.dianping.puma.biz.entity.PumaTaskEntity;
+
+import java.util.List;
+
+public class PolledTaskChecker extends AbstractTaskChecker {
+
+	private List<PumaTaskEntity> pumaTasks;
+
+	@Override
+	protected void doStart() {
+
+	}
+
+	@Override
+	protected void doStop() {
+
+	}
+
+
+}

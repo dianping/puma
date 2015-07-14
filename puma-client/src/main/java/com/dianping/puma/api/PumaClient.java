7f1620fd26e1b565ad46c1673fce9324037a7611
Dozer <mail@dozer.cc>
2015-07-14 14:38:47 +0800

    finish new puma client

--- puma-client/src/main/java/com/dianping/puma/api/PumaClient.java
+++ puma-client/src/main/java/com/dianping/puma/api/PumaClient.java
@@ -1,31 +1,26 @@
 package com.dianping.puma.api;
 
-import com.dianping.puma.api.exception.PumaClientAuthException;
 import com.dianping.puma.api.exception.PumaClientException;
-import com.dianping.puma.core.model.BinlogInfo;
 import com.dianping.puma.core.dto.BinlogMessage;
+import com.dianping.puma.core.model.BinlogInfo;
 
 import java.util.concurrent.TimeUnit;
 
 public interface PumaClient {
 
-    void connect() throws PumaClientException;
+    BinlogMessage get(int batchSize) throws PumaClientException;
 
-    void disconnect() throws PumaClientException;
+    BinlogMessage get(int batchSize, long timeout, TimeUnit timeUnit) throws PumaClientException;
 
-    BinlogMessage get(int batchSize) throws PumaClientException, PumaClientAuthException;
+    BinlogMessage getWithAck(int batchSize) throws PumaClientException;
 
-    BinlogMessage get(int batchSize, long timeout, TimeUnit timeUnit) throws PumaClientException, PumaClientAuthException;
+    BinlogMessage getWithAck(int batchSize, long timeout, TimeUnit timeUnit) throws PumaClientException;
 
-    BinlogMessage getWithAck(int batchSize) throws PumaClientException, PumaClientAuthException;
+    void ack(BinlogInfo binlogInfo) throws PumaClientException;
 
-    BinlogMessage getWithAck(int batchSize, long timeout, TimeUnit timeUnit) throws PumaClientException, PumaClientAuthException;
+    void rollback(BinlogInfo binlogInfo) throws PumaClientException;
 
-    void ack(BinlogInfo binlogInfo) throws PumaClientException, PumaClientAuthException;
+    void rollback() throws PumaClientException;
 
-    void rollback(BinlogInfo binlogInfo) throws PumaClientException, PumaClientAuthException;
-
-    void rollback() throws PumaClientException, PumaClientAuthException;
-
-    void subscribe(boolean dml, boolean ddl, boolean transaction, String database, String... tables) throws PumaClientException;
+    void subscribe(boolean dml, boolean ddl, boolean transaction, String database, String... tables);
 }

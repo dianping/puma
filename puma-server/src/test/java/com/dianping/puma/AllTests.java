fe95be844b0cc47ad9a3caa438af2fcf46e8ba61
Dozer <mail@dozer.cc>
2015-07-22 13:37:54 +0800

    replace with qps counter

--- puma-server/src/test/java/com/dianping/puma/AllTests.java
+++ puma-server/src/test/java/com/dianping/puma/AllTests.java
@@ -1,21 +1,8 @@
 package com.dianping.puma;
 
+import com.dianping.puma.filter.*;
 import com.dianping.puma.pumaserver.channel.impl.DefaultAsyncBinlogChannelTest;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-import com.dianping.puma.filter.DDLEventFilterTest;
-import com.dianping.puma.filter.DMLEventFilterTest;
-import com.dianping.puma.filter.DbTbEventFilterTest;
-import com.dianping.puma.filter.DefaultEventFilterChainTest;
-import com.dianping.puma.filter.DmlDdlEventFilterTest;
-import com.dianping.puma.filter.EventFilterChainFactoryTest;
-import com.dianping.puma.filter.StorageEventFilterChainTest;
-import com.dianping.puma.filter.TransactionEventFilterTest;
-import com.dianping.puma.filter.TransactionInfoEventFilterTest;
-import com.dianping.puma.status.SecondBucketCounterTest;
+import com.dianping.puma.status.QpsCounterTest;
 import com.dianping.puma.storage.DefaultArchiveStrategyTest;
 import com.dianping.puma.storage.DefaultBucketManagerTest;
 import com.dianping.puma.storage.DefaultCleanupStrategyTest;
@@ -26,42 +13,45 @@ import com.dianping.puma.storage.index.DefaultDataIndexTest;
 import com.dianping.puma.storage.index.L2IndexItemConvertorTest;
 import com.dianping.puma.storage.index.LocalFileIndexBucketTest;
 import com.dianping.puma.utils.PacketUtilTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 //
-      // filter
-      DbTbEventFilterTest.class, //
-      DDLEventFilterTest.class,//
-      DefaultEventFilterChainTest.class,//
-      DmlDdlEventFilterTest.class, //
-      DMLEventFilterTest.class,//
-      EventFilterChainFactoryTest.class,//
-      StorageEventFilterChainTest.class,//
-      TransactionEventFilterTest.class, //
-      TransactionInfoEventFilterTest.class,//
+        // filter
+        DbTbEventFilterTest.class, //
+        DDLEventFilterTest.class,//
+        DefaultEventFilterChainTest.class,//
+        DmlDdlEventFilterTest.class, //
+        DMLEventFilterTest.class,//
+        EventFilterChainFactoryTest.class,//
+        StorageEventFilterChainTest.class,//
+        TransactionEventFilterTest.class, //
+        TransactionInfoEventFilterTest.class,//
 
-      //status
-      SecondBucketCounterTest.class,//
-      
-      // storage.bucket
-      LocalBucketIndexTest.class, //
-      LocalBucketTest.class, //
+        //status
+        QpsCounterTest.class,//
 
-      // storage.index
-      DefaultDataIndexTest.class,//
-      L2IndexItemConvertorTest.class,//
-      LocalFileIndexBucketTest.class,//
+        // storage.bucket
+        LocalBucketIndexTest.class, //
+        LocalBucketTest.class, //
 
-      // storage
-      DefaultArchiveStrategyTest.class,//
-      DefaultBucketManagerTest.class,//
-      DefaultCleanupStrategyTest.class,//
-      SequenceTest.class,//
+        // storage.index
+        DefaultDataIndexTest.class,//
+        L2IndexItemConvertorTest.class,//
+        LocalFileIndexBucketTest.class,//
 
-      // utils
-      PacketUtilTest.class, //
-      DefaultDataIndexTest.class, //
+        // storage
+        DefaultArchiveStrategyTest.class,//
+        DefaultBucketManagerTest.class,//
+        DefaultCleanupStrategyTest.class,//
+        SequenceTest.class,//
+
+        // utils
+        PacketUtilTest.class, //
+        DefaultDataIndexTest.class, //
         DefaultAsyncBinlogChannelTest.class
 })
 public class AllTests {

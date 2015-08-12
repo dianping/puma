5c22e4b3d3f6abb03d4081c7bec01e15803bd648
Dozer <mail@dozer.cc>
2015-08-12 14:56:35 +0800

    improve simple puma client

--- puma-client/src/test/java/com/dianping/puma/api/impl/ClusterPumaClientMainTest.java
+++ puma-client/src/test/java/com/dianping/puma/api/impl/ClusterPumaClientMainTest.java
@@ -2,22 +2,39 @@ package com.dianping.puma.api.impl;
 
 import com.dianping.puma.api.PumaClient;
 import com.dianping.puma.api.PumaClientConfig;
+import com.dianping.puma.core.dto.BinlogMessage;
+import com.dianping.puma.core.event.Event;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ClusterPumaClientMainTest {
 
-	public static void main(String[] args) {
-		List<String> tables = new ArrayList<String>();
-		tables.add("puma-test-tb");
+    public static void main(String[] args) {
+        List<String> tables = new ArrayList<String>();
+        tables.add("debug");
 
-		PumaClient client = new PumaClientConfig()
-				.setClientName("puma-test")
-				.setDatabase("test-database")
-				.setTables(tables)
-				.buildClusterPumaClient();
+        PumaClient client = new PumaClientConfig()
+                .setClientName("puma-perd-test")
+                .setDatabase("test")
+                .setTables(tables)
+                .buildClusterPumaClient();
 
-		client.get(10);
-	}
+        final int size = 100;
+
+        while (true) {
+            try {
+                BinlogMessage message = client.get(size, 5, TimeUnit.SECONDS);
+
+                for (Event event : message.getBinlogEvents()) {
+                    System.out.println(event.toString());
+                }
+
+                client.ack(message.getLastBinlogInfo());
+            } catch (Exception exp) {
+                exp.printStackTrace();
+            }
+        }
+    }
 }

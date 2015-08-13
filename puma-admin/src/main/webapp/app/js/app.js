9e7b65194d913b875b70e89f89db82aa00707bc6
xiaotian.li <xiaotian.li@dianping.com>
2015-08-13 15:06:11 +0800

    revert web puma server list

--- puma-admin/src/main/webapp/app/js/app.js
+++ puma-admin/src/main/webapp/app/js/app.js
@@ -1,9 +1,9 @@
-var puma = angular.module('puma', ['ngRoute']);
-
-puma.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/puma-server/list', {
-    templateUrl: 'app/partials/puma-server/list.html',
-    controller: 'controller.js'
-  }).otherwise('index.html');
-}]);
+//var puma = angular.module('puma', ['ngRoute']);
+//
+//puma.config(['$routeProvider', function($routeProvider) {
+//  $routeProvider.when('/puma-server/list', {
+//    templateUrl: 'app/partials/puma-server/list.html',
+//    controller: 'controller.js'
+//  }).otherwise('index.html');
+//}]);
 

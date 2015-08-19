ad09db3dff1c5a2610c84d755e1a7d31b83d9b27
Dozer <mail@dozer.cc>
2015-08-19 19:10:09 +0800

    try to fix default event channel bug

--- puma-server/src/main/java/com/dianping/puma/storage/channel/DefaultEventChannel.java
+++ puma-server/src/main/java/com/dianping/puma/storage/channel/DefaultEventChannel.java
@@ -109,17 +109,14 @@ public class DefaultEventChannel extends AbstractEventChannel implements EventCh
 
 				if (this.tables != null && !this.tables.contains(nextL2Index.getTable()) && !nextL2Index.isTransaction()) {
 					lastIndexKey = nextL2Index.getIndexKey();
-					lastReadSequence = sequence;
 					continue;
 				}
 				if (this.withDdl != nextL2Index.isDdl() && this.withDml != nextL2Index.isDml()) {
 					lastIndexKey = nextL2Index.getIndexKey();
-					lastReadSequence = sequence;
 					continue;
 				}
 				if (!this.withTransaction && nextL2Index.isTransaction()) {
 					lastIndexKey = nextL2Index.getIndexKey();
-					lastReadSequence = sequence;
 					continue;
 				}
 

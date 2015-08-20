48b89a8f5b2d1ec63355ac9c8ec5ec4efea0d67c
Dozer <mail@dozer.cc>
2015-08-20 18:53:42 +0800

    fix no storage problem

--- puma-server/src/main/java/com/dianping/puma/sender/FileDumpSender.java
+++ puma-server/src/main/java/com/dianping/puma/sender/FileDumpSender.java
@@ -1,12 +1,12 @@
 /**
  * Project: puma-server
- * 
+ * <p/>
  * File Created at 2012-7-7
  * $Id$
- * 
+ * <p/>
  * Copyright 2010 dianping.com.
  * All rights reserved.
- *
+ * <p/>
  * This software is the confidential and proprietary information of
  * Dianping Company. ("Confidential Information").  You shall not
  * disclose such Confidential Information and shall use it only in
@@ -14,10 +14,6 @@
  * with dianping.com.
  */
 package com.dianping.puma.sender;
-
-import java.util.Map;
-
-import org.jboss.netty.util.internal.ConcurrentHashMap;
 
 import com.dianping.cat.Cat;
 import com.dianping.puma.common.PumaContext;
@@ -32,194 +28,205 @@ import com.dianping.puma.storage.EventStorage;
 import com.dianping.puma.storage.bucket.LocalFileDataBucketManager;
 import com.dianping.puma.storage.exception.StorageException;
 import com.dianping.puma.storage.exception.StorageLifeCycleException;
+import org.jboss.netty.util.internal.ConcurrentHashMap;
+
+import java.util.Map;
 
 /**
- * 
+ *
  * @author Leo Liang
- * 
+ *
  */
 public class FileDumpSender extends AbstractSender {
-	private Map<String, DefaultEventStorage> storages = new ConcurrentHashMap<String, DefaultEventStorage>();
+    private Map<String, DefaultEventStorage> storages = new ConcurrentHashMap<String, DefaultEventStorage>();
 
-	private ChangedEvent transactionBegin;
+    private ChangedEvent transactionBegin;
 
-	private EventFilterChain storageEventFilterChain;
+    private EventFilterChain storageEventFilterChain;
 
-	private String taskName;
+    private String taskName;
 
-	private String binlogIndexBaseDir;
+    private String binlogIndexBaseDir;
 
-	private String masterStorageBaseDir;
+    private String masterStorageBaseDir;
 
-	private String masterBucketFilePrefix;
+    private String masterBucketFilePrefix;
 
-	private String slaveStorageBaseDir;
+    private String slaveStorageBaseDir;
 
-	private String slaveBucketFilePrefix;
+    private String slaveBucketFilePrefix;
 
-	private int maxMasterFileCount;
+    private int maxMasterFileCount;
 
-	private int maxMasterBucketLengthMB;
+    private int maxMasterBucketLengthMB;
 
-	private int maxSlaveBucketLengthMB;
+    private int maxSlaveBucketLengthMB;
 
-	private int preservedDay;
+    private int preservedDay;
 
-	private EventCodec codec;
+    private EventCodec codec;
 
-	@Override
-	public void start() throws Exception {
-		super.start();
-	}
+    @Override
+    public void start() throws Exception {
+        super.start();
+    }
 
-	@Override
-	public void stop() throws Exception {
-		super.stop();
-	}
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
 
-	@Override
-	protected void doSend(ChangedEvent event, PumaContext context) throws SenderException {
-		// Storage filter.
-		storageEventFilterChain.reset();
-		if (!storageEventFilterChain.doNext(event)) {
-			return;
-		}
+    @Override
+    protected void doSend(ChangedEvent event, PumaContext context) throws SenderException {
+        // Storage filter.
+        storageEventFilterChain.reset();
+        if (!storageEventFilterChain.doNext(event)) {
+            return;
+        }
 
-		try {
-			String database = event.getDatabase();
+        try {
+            String database = event.getDatabase();
 
-			if (database != null && database.length() > 0) {
-				DefaultEventStorage eventStorage = storages.get(database);
+            if (database != null && database.length() > 0) {
+                DefaultEventStorage eventStorage = storages.get(database);
 
-				if (eventStorage == null) {
-					eventStorage = buildEventStorage(database);
+                if (eventStorage == null) {
+                    eventStorage = buildEventStorage(database);
+                    storages.put(database, eventStorage);
+                }
 
-					storages.put(database, eventStorage);
-				}
+                boolean isTransactionBegin = false;
 
-				boolean isTransactionBegin = false;
+                if (event instanceof RowChangedEvent) {
+                    isTransactionBegin = ((RowChangedEvent) event).isTransactionBegin();
+                }
 
-				if (event instanceof RowChangedEvent) {
-					isTransactionBegin = ((RowChangedEvent) event).isTransactionBegin();
-				}
+                if (transactionBegin != null && !isTransactionBegin) {
+                    eventStorage.store(transactionBegin);
+                    transactionBegin = null;
+                }
 
-				if (transactionBegin != null && !isTransactionBegin) {
-					eventStorage.store(transactionBegin);
-					transactionBegin = null;
-				}
+                eventStorage.store(event);
+            } else {
+                if (event instanceof RowChangedEvent) {
+                    if (((RowChangedEvent) event).isTransactionBegin()) {
+                        transactionBegin = event;
+                    } else {
+                        Cat.logEvent("Puma", "RowChangeEvent-Has-No-Database");
+                        LOG.error(String.format("RowChangeEvent[%s] has no database", event.toString()));
+                    }
+                } else {
+                    Cat.logEvent("Puma", "ChangeEvent-Has-No-Database");
+                    LOG.error(String.format("ChangeEvent[%s] has no database", event.toString()));
+                }
+            }
+        } catch (StorageException e) {
+            throw new SenderException("FileDumpSender.doSend failed.", e);
+        }
+    }
 
-				eventStorage.store(event);
-			} else {
-				if (event instanceof RowChangedEvent) {
-					if (((RowChangedEvent) event).isTransactionBegin()) {
-						transactionBegin = event;
-					} else {
-						Cat.logEvent("Puma", "RowChangeEvent-Has-No-Database");
-						LOG.error(String.format("RowChangeEvent[%s] has no database", event.toString()));
-					}
-				} else {
-					Cat.logEvent("Puma", "ChangeEvent-Has-No-Database");
-					LOG.error(String.format("ChangeEvent[%s] has no database", event.toString()));
-				}
-			}
-		} catch (StorageException e) {
-			throw new SenderException("FileDumpSender.doSend failed.", e);
-		}
-	}
+    private DefaultEventStorage buildEventStorage(String database) throws StorageLifeCycleException {
+        DefaultEventStorage storage = new DefaultEventStorage();
+        storage.setName("storage-" + database);
+        storage.setTaskName(taskName);
+        storage.setCodec(codec);
 
-	private DefaultEventStorage buildEventStorage(String database) throws StorageLifeCycleException {
-		DefaultEventStorage storage = new DefaultEventStorage();
-		storage.setName("storage-" + database);
-		storage.setTaskName(taskName);
-		storage.setCodec(codec);
+        // File sender master storage.
+        LocalFileDataBucketManager masterBucketIndex = new LocalFileDataBucketManager();
+        masterBucketIndex.setBaseDir(masterStorageBaseDir + "/" + database);
+        masterBucketIndex.setBucketFilePrefix(masterBucketFilePrefix);
+        masterBucketIndex.setMaxBucketLengthMB(maxMasterBucketLengthMB);
 
-		// File sender master storage.
-		LocalFileDataBucketManager masterBucketIndex = new LocalFileDataBucketManager();
-		masterBucketIndex.setBaseDir(masterStorageBaseDir + "/" + database);
-		masterBucketIndex.setBucketFilePrefix(masterBucketFilePrefix);
-		masterBucketIndex.setMaxBucketLengthMB(maxMasterBucketLengthMB);
+        storage.setMasterBucketIndex(masterBucketIndex);
 
-		storage.setMasterBucketIndex(masterBucketIndex);
+        // File sender slave storage.
+        LocalFileDataBucketManager slaveBucketIndex = new LocalFileDataBucketManager();
+        slaveBucketIndex.setBaseDir(slaveStorageBaseDir + "/" + database);
+        slaveBucketIndex.setBucketFilePrefix(slaveBucketFilePrefix);
+        slaveBucketIndex.setMaxBucketLengthMB(maxSlaveBucketLengthMB);
 
-		// File sender slave storage.
-		LocalFileDataBucketManager slaveBucketIndex = new LocalFileDataBucketManager();
-		slaveBucketIndex.setBaseDir(slaveStorageBaseDir + "/" + database);
-		slaveBucketIndex.setBucketFilePrefix(slaveBucketFilePrefix);
-		slaveBucketIndex.setMaxBucketLengthMB(maxSlaveBucketLengthMB);
+        storage.setSlaveBucketIndex(slaveBucketIndex);
 
-		storage.setSlaveBucketIndex(slaveBucketIndex);
+        // Archive strategy.
+        DefaultArchiveStrategy archiveStrategy = new DefaultArchiveStrategy();
+        archiveStrategy.setServerName(taskName);
+        archiveStrategy.setMaxMasterFileCount(maxMasterFileCount);
+        storage.setArchiveStrategy(archiveStrategy);
 
-		// Archive strategy.
-		DefaultArchiveStrategy archiveStrategy = new DefaultArchiveStrategy();
-		archiveStrategy.setServerName(taskName);
-		archiveStrategy.setMaxMasterFileCount(maxMasterFileCount);
-		storage.setArchiveStrategy(archiveStrategy);
+        // Clean up strategy.
+        DefaultCleanupStrategy cleanupStrategy = new DefaultCleanupStrategy();
+        cleanupStrategy.setPreservedDay(preservedDay);
+        storage.setCleanupStrategy(cleanupStrategy);
 
-		// Clean up strategy.
-		DefaultCleanupStrategy cleanupStrategy = new DefaultCleanupStrategy();
-		cleanupStrategy.setPreservedDay(preservedDay);
-		storage.setCleanupStrategy(cleanupStrategy);
+        storage.setBinlogIndexBaseDir(binlogIndexBaseDir + database);
 
-		storage.setBinlogIndexBaseDir(binlogIndexBaseDir + database);
+        storage.start();
+        return storage;
+    }
 
-		storage.start();
-		return storage;
-	}
+    public void setStorageEventFilterChain(EventFilterChain storageEventFilterChain) {
+        this.storageEventFilterChain = storageEventFilterChain;
+    }
 
-	public void setStorageEventFilterChain(EventFilterChain storageEventFilterChain) {
-		this.storageEventFilterChain = storageEventFilterChain;
-	}
+    public void setStorages(Map<String, DefaultEventStorage> storages) {
+        this.storages = storages;
+    }
 
-	public void setStorages(Map<String, DefaultEventStorage> storages) {
-		this.storages = storages;
-	}
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
 
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
+    public void setBinlogIndexBaseDir(String binlogIndexBaseDir) {
+        this.binlogIndexBaseDir = binlogIndexBaseDir;
+    }
 
-	public void setBinlogIndexBaseDir(String binlogIndexBaseDir) {
-		this.binlogIndexBaseDir = binlogIndexBaseDir;
-	}
+    public void setMasterStorageBaseDir(String masterStorageBaseDir) {
+        this.masterStorageBaseDir = masterStorageBaseDir;
+    }
 
-	public void setMasterStorageBaseDir(String masterStorageBaseDir) {
-		this.masterStorageBaseDir = masterStorageBaseDir;
-	}
+    public void setSlaveStorageBaseDir(String slaveStorageBaseDir) {
+        this.slaveStorageBaseDir = slaveStorageBaseDir;
+    }
 
-	public void setSlaveStorageBaseDir(String slaveStorageBaseDir) {
-		this.slaveStorageBaseDir = slaveStorageBaseDir;
-	}
+    public void setMaxMasterBucketLengthMB(int maxMasterBucketLengthMB) {
+        this.maxMasterBucketLengthMB = maxMasterBucketLengthMB;
+    }
 
-	public void setMaxMasterBucketLengthMB(int maxMasterBucketLengthMB) {
-		this.maxMasterBucketLengthMB = maxMasterBucketLengthMB;
-	}
+    public void setMaxSlaveBucketLengthMB(int maxSlaveBucketLengthMB) {
+        this.maxSlaveBucketLengthMB = maxSlaveBucketLengthMB;
+    }
 
-	public void setMaxSlaveBucketLengthMB(int maxSlaveBucketLengthMB) {
-		this.maxSlaveBucketLengthMB = maxSlaveBucketLengthMB;
-	}
+    public void setSlaveBucketFilePrefix(String slaveBucketFilePrefix) {
+        this.slaveBucketFilePrefix = slaveBucketFilePrefix;
+    }
 
-	public void setSlaveBucketFilePrefix(String slaveBucketFilePrefix) {
-		this.slaveBucketFilePrefix = slaveBucketFilePrefix;
-	}
+    public void setCodec(EventCodec codec) {
+        this.codec = codec;
+    }
 
-	public void setCodec(EventCodec codec) {
-		this.codec = codec;
-	}
+    public void setMasterBucketFilePrefix(String masterBucketFilePrefix) {
+        this.masterBucketFilePrefix = masterBucketFilePrefix;
+    }
 
-	public void setMasterBucketFilePrefix(String masterBucketFilePrefix) {
-		this.masterBucketFilePrefix = masterBucketFilePrefix;
-	}
+    public void setMaxMasterFileCount(int maxMasterFileCount) {
+        this.maxMasterFileCount = maxMasterFileCount;
+    }
 
-	public void setMaxMasterFileCount(int maxMasterFileCount) {
-		this.maxMasterFileCount = maxMasterFileCount;
-	}
+    public void setPreservedDay(int preservedDay) {
+        this.preservedDay = preservedDay;
+    }
 
-	public void setPreservedDay(int preservedDay) {
-		this.preservedDay = preservedDay;
-	}
-
-	@Override
-	public EventStorage getStorage(String database) {
-		return storages.get(database);
-	}
+    @Override
+    public EventStorage getStorage(String database) {
+        try {
+            DefaultEventStorage eventStorage = storages.get(database);
+            if (eventStorage == null) {
+                eventStorage = buildEventStorage(database);
+                storages.put(database, eventStorage);
+            }
+            return eventStorage;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
--- puma-server/src/main/java/com/dianping/puma/server/container/TaskContainer.java
+++ puma-server/src/main/java/com/dianping/puma/server/container/TaskContainer.java
@@ -1,26 +1,26 @@
 package com.dianping.puma.server.container;
 
-import java.util.List;
-
 import com.dianping.puma.biz.entity.PumaTaskEntity;
 import com.dianping.puma.storage.EventStorage;
 import com.dianping.puma.taskexecutor.TaskExecutor;
 
+import java.util.List;
+
 public interface TaskContainer {
 
-	public TaskExecutor get(String taskId);
+    TaskExecutor get(String taskId);
 
-	public List<TaskExecutor> getAll();
+    List<TaskExecutor> getAll();
 
-	public EventStorage getTaskStorage(String database);
+    EventStorage getTaskStorage(String database);
 
-	public void create(String name, PumaTaskEntity task);
+    void create(String name, PumaTaskEntity task);
 
-	public void update(String name, PumaTaskEntity oriTask, PumaTaskEntity task);
+    void update(String name, PumaTaskEntity oriTask, PumaTaskEntity task);
 
-	public void delete(String name, PumaTaskEntity task);
+    void delete(String name, PumaTaskEntity task);
 
-	public void start(String name);
+    void start(String name);
 
-	public void stop(String name);
+    void stop(String name);
 }

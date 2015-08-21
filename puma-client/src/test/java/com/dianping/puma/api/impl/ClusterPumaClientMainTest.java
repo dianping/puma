38f4686cde4e1d965d8a762d4a6e3713af74c120
Dozer <mail@dozer.cc>
2015-08-21 14:43:21 +0800

    change test

--- puma-client/src/test/java/com/dianping/puma/api/impl/ClusterPumaClientMainTest.java
+++ puma-client/src/test/java/com/dianping/puma/api/impl/ClusterPumaClientMainTest.java
@@ -13,23 +13,31 @@ public class ClusterPumaClientMainTest {
 
     public static void main(String[] args) {
         List<String> tables = new ArrayList<String>();
-        tables.add("UOD_Order0");
-        tables.add("UOD_Order1");
-        tables.add("UOD_OrderAdditionalFee0");
-        tables.add("UOD_OrderAdditionalFee1");
+        tables.add("UOD_Order2");
+        tables.add("UOD_Order3");
+        tables.add("UOD_OrderExtraFields2");
+        tables.add("UOD_OrderExtraFields3");
+        tables.add("UOD_OrderLog2");
+        tables.add("UOD_OrderLog3");
+        tables.add("UOD_OrderPaymentDetail2");
+        tables.add("UOD_OrderPaymentDetail3");
+        tables.add("UOD_OrderSKU2");
+        tables.add("UOD_OrderSKU3");
+        tables.add("UOD_OrderSKUExtraFields2");
+        tables.add("UOD_OrderSKUExtraFields3");
 
         PumaClient client = new PumaClientConfig()
                 .setClientName("dozer-debug")
-                .setDatabase("UnifiedOrder0")
+                .setDatabase("UnifiedOrder1")
                 .setTables(tables)
-                .setServerHost("127.0.0.1:4040")
+                .setServerHost("192.168.216.79:4040")
                 .buildSimplePumaClient();
 
         final int size = 100;
 
         while (true) {
             try {
-                BinlogMessage message = client.get(size, 5, TimeUnit.SECONDS);
+                BinlogMessage message = client.get(size, 100, TimeUnit.SECONDS);
 
                 for (Event event : message.getBinlogEvents()) {
                     System.out.println(event.toString());

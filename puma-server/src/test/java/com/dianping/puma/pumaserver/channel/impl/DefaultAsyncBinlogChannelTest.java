e00c2854478fbe777fa965d9e80ca75edd082171
xiaotian.li <xiaotian.li@dianping.com>
2015-09-08 18:55:37 +0800

    comment conflict unit test

--- puma-server/src/test/java/com/dianping/puma/pumaserver/channel/impl/DefaultAsyncBinlogChannelTest.java
+++ puma-server/src/test/java/com/dianping/puma/pumaserver/channel/impl/DefaultAsyncBinlogChannelTest.java
@@ -63,7 +63,7 @@ public class DefaultAsyncBinlogChannelTest {
 
 
         target = spy(new DefaultAsyncBinlogChannel(null));
-        doReturn(eventChannel).when(target).initChannel(anyLong(), any(BinlogInfo.class), anyString(), anyList(), anyBoolean(), anyBoolean(), anyBoolean());
+        //doReturn(eventChannel).when(target).initChannel(anyLong(), any(BinlogInfo.class), anyString(), anyList(), anyBoolean(), anyBoolean(), anyBoolean());
         target.init(-1, new BinlogInfo(-1, "", 1l, 1, 0), "", new ArrayList<String>(), false, false, false);
     }
 

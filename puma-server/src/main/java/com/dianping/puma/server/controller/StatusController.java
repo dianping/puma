9356c98309ab5071785205b292d8f9f30f757fbe
Dozer <mail@dozer.cc>
2015-09-09 11:50:55 +0800

    change home page

--- puma-server/src/main/java/com/dianping/puma/server/controller/StatusController.java
+++ puma-server/src/main/java/com/dianping/puma/server/controller/StatusController.java
@@ -1,19 +1,23 @@
 package com.dianping.puma.server.controller;
 
 import com.dianping.puma.status.SystemStatusManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/status")
 public class StatusController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     @ResponseBody
     public Object index() {
+        return SystemStatusManager.getStatus();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/status")
+    @ResponseBody
+    public Object status() {
         return SystemStatusManager.getStatus();
     }
 }

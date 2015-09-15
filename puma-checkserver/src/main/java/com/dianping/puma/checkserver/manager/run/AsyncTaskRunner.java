31bb9368293da405624b9ad47dc1a6c86cde6044
xiaotian.li <xiaotian.li@dianping.com>
2015-09-15 18:48:08 +0800

    change single task dispatcher to multiple task dispatcher

--- puma-checkserver/src/main/java/com/dianping/puma/checkserver/manager/run/AsyncTaskRunner.java
+++ puma-checkserver/src/main/java/com/dianping/puma/checkserver/manager/run/AsyncTaskRunner.java
@@ -2,22 +2,36 @@ package com.dianping.puma.checkserver.manager.run;
 
 import com.dianping.puma.biz.entity.CheckTaskEntity;
 import com.dianping.puma.checkserver.TaskExecutor;
-import com.dianping.puma.checkserver.manager.utils.ThreadPool;
 import com.dianping.puma.checkserver.model.TaskEntity;
 import com.dianping.puma.checkserver.model.TaskResult;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 @Service
 public class AsyncTaskRunner implements TaskRunner {
+
+    private final int threadPoolSize = 5;
+
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(threadPoolSize, new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable runnable) {
+            Thread thread = new Thread(runnable);
+            thread.setName("async-task-runner");
+            thread.setDaemon(true);
+            return thread;
+        }
+    });
 
     @Override
     public TaskRunFuture run(CheckTaskEntity checkTask, TaskRunFutureListener listener) {
         Callable<TaskResult> taskExecutor = build(checkTask);
 
         TaskRunFuture taskRunFuture = new TaskRunFuture(taskExecutor, listener);
-        ThreadPool.execute(taskRunFuture);
+        threadPool.execute(taskRunFuture);
         return taskRunFuture;
     }
 

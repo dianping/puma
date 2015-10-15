cac90771822d0519673f513802fe7e59bd6a39a9
Dozer <mail@dozer.cc>
2015-10-15 16:30:36 +0800

    add docs

--- puma-client/src/main/java/com/dianping/puma/api/PumaClient.java
+++ puma-client/src/main/java/com/dianping/puma/api/PumaClient.java
@@ -7,17 +7,57 @@ import java.util.concurrent.TimeUnit;
 
 public interface PumaClient {
 
+    /**
+     * 获得一批数据
+     * 该方法会一直等待，直到服务器新数据量达到 batchSize 后返回
+     * @param batchSize
+     * @return 满足 batchSize 的数据
+     * @throws PumaClientException
+     */
     BinlogMessage get(int batchSize) throws PumaClientException;
 
+    /**
+     * 获得一批数据，并设置超时时间
+     * 如果超过超时时间或者达到 batchSize，该方法就会返回。
+     * @param batchSize
+     * @param timeout
+     * @param timeUnit
+     * @return 有多少新数据就返回多少新数据
+     * @throws PumaClientException
+     */
     BinlogMessage get(int batchSize, long timeout, TimeUnit timeUnit) throws PumaClientException;
 
+    /**
+     * 得到数据后自动 ack，使用该方法可能会在极少数情况下丢失数据
+     * 只有当不太关心数据准确性的情况下才使用
+     * @param batchSize
+     * @return
+     * @throws PumaClientException
+     */
     BinlogMessage getWithAck(int batchSize) throws PumaClientException;
 
+    /**
+     * 得到数据后自动 ack，使用该方法可能会在极少数情况下丢失数据
+     * 只有当不太关心数据准确性的情况下才使用
+     * @param batchSize
+     * @param timeout
+     * @param timeUnit
+     * @return
+     * @throws PumaClientException
+     */
     BinlogMessage getWithAck(int batchSize, long timeout, TimeUnit timeUnit) throws PumaClientException;
 
+    /**
+     * 通知服务器客户端当前最后处理完成的数据位置，遇到问题重新连接后，会从这个位置开始继续接收数据
+     * @param binlogInfo
+     * @throws PumaClientException
+     */
     void ack(BinlogInfo binlogInfo) throws PumaClientException;
 
+    /**
+     * 会滚到制定位置重新开始
+     * @param binlogInfo 可以指定 binlog 位置或直接指定时间
+     * @throws PumaClientException
+     */
     void rollback(BinlogInfo binlogInfo) throws PumaClientException;
-
-    void rollback() throws PumaClientException;
 }

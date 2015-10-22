112c6992f7f5883709107cf97b59388dc3a170b1
xiaotian.li <xiaotian.li@dianping.com>
2015-10-22 10:20:14 +0800

    add write data manager to data manager factory

--- puma/src/main/java/com/dianping/puma/storage/data/DataManagerFactory.java
+++ puma/src/main/java/com/dianping/puma/storage/data/DataManagerFactory.java
@@ -1,6 +1,7 @@
 package com.dianping.puma.storage.data;
 
 import com.dianping.puma.storage.data.impl.SingleReadDataManager;
+import com.dianping.puma.storage.data.impl.SingleWriteDataManager;
 
 public class DataManagerFactory {
 
@@ -8,7 +9,15 @@ public class DataManagerFactory {
 
 	private static final int readAvgSizeByte = 16 * 1024 * 1024; // 64M.
 
+	private static final int writeBufSizeByte = 64 * 1024 * 1024; // 64M.
+
+	private static final int writeMaxSizeByte = 1024 * 1024 * 1024; // 1G.
+
 	private static SingleReadDataManager newSingleReadDataManager(String filename) {
 		return new SingleReadDataManager(filename, readBufSizeByte, readAvgSizeByte);
+	}
+
+	private static SingleWriteDataManager newSingleWriteDataManager(String filename) {
+		return new SingleWriteDataManager(filename, writeBufSizeByte, writeMaxSizeByte);
 	}
 }

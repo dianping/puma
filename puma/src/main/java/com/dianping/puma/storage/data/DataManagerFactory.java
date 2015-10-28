dad1ed38fda3bf65bdde97a7ffe4809f7b7236a7
Dozer <mail@dozer.cc>
2015-10-28 18:36:57 +0800

    change buffer size

--- puma/src/main/java/com/dianping/puma/storage/data/DataManagerFactory.java
+++ puma/src/main/java/com/dianping/puma/storage/data/DataManagerFactory.java
@@ -4,22 +4,22 @@ import java.io.File;
 
 public final class DataManagerFactory {
 
-	private static final int READ_BUF_SIZE_BYTE = 64 * 1024 * 1024; // 64M.
+    private static final int READ_BUF_SIZE_BYTE = 1024 * 1024; // 1M.
 
-	private static final int READ_AVG_SIZE_BYTE = 16 * 1024 * 1024; // 64M.
+    private static final int READ_AVG_SIZE_BYTE = 16 * 1024 * 1024; // 64M.
 
-	private static final int WRITE_BUF_SIZE_BYTE = 16 * 1024 * 1024; // 64M.
+    private static final int WRITE_BUF_SIZE_BYTE = 1024 * 1024; // 1M.
 
-	private static final int WRITE_MAX_SIZE_BYTE = 1024 * 1024 * 1024; // 1G.
+    private static final int WRITE_MAX_SIZE_BYTE = 1024 * 1024 * 1024; // 1G.
 
-	private DataManagerFactory() {
-	}
+    private DataManagerFactory() {
+    }
 
-	public static SingleReadDataManager newSingleReadDataManager(File file) {
-		return new SingleReadDataManager(file, READ_BUF_SIZE_BYTE, READ_AVG_SIZE_BYTE);
-	}
+    public static SingleReadDataManager newSingleReadDataManager(File file) {
+        return new SingleReadDataManager(file, READ_BUF_SIZE_BYTE, READ_AVG_SIZE_BYTE);
+    }
 
-	public static SingleWriteDataManager newSingleWriteDataManager(File file, String date, int number) {
-		return new SingleWriteDataManager(file, date, number, WRITE_BUF_SIZE_BYTE, WRITE_MAX_SIZE_BYTE);
-	}
+    public static SingleWriteDataManager newSingleWriteDataManager(File file, String date, int number) {
+        return new SingleWriteDataManager(file, date, number, WRITE_BUF_SIZE_BYTE, WRITE_MAX_SIZE_BYTE);
+    }
 }

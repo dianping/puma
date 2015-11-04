bdeffc38e4afed4ccff49a641e6523f989f350a2
xiaotian.li <xiaotian.li@dianping.com>
2015-11-04 17:51:38 +0800

    fix file delete bugs

--- puma/src/main/java/com/dianping/puma/storage/manage/LocalFileDatabaseStorageManager.java
+++ puma/src/main/java/com/dianping/puma/storage/manage/LocalFileDatabaseStorageManager.java
@@ -1,5 +1,6 @@
 package com.dianping.puma.storage.manage;
 
+import com.dianping.puma.storage.filesystem.FileSystem;
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 
@@ -9,18 +10,25 @@ import java.io.IOException;
 @Service
 public class LocalFileDatabaseStorageManager implements DatabaseStorageManager {
 
-	private static final String BINLOG_INDEX_ROOT_PATH = "/data/appdatas/puma/binlogIndex/";
+	private static final String BINLOG_L1_INDEX_ROOT_PATH = FileSystem.getL1IndexDir().getAbsolutePath();
 
-	private static final String BINLOG_MASTER_STORAGE_ROOT_PATH = "/data/appdatas/puma/storage/master/";
+	private static final String BINLOG_L2_INDEX_ROOT_PATH = FileSystem.getL2IndexDir().getAbsolutePath();
 
-	private static final String BINLOG_SLAVE_STORAGE_ROOT_PATH = "/data/appdatas/puma/storage/slave";
+	private static final String BINLOG_MASTER_STORAGE_ROOT_PATH = FileSystem.getMasterDataDir().getAbsolutePath();
+
+	private static final String BINLOG_SLAVE_STORAGE_ROOT_PATH = FileSystem.getSlaveDataDir().getAbsolutePath();
 
 	@Override
 	public void delete(String database) {
 		try {
-			File binlogIndexFile = new File(BINLOG_INDEX_ROOT_PATH, database);
-			if (binlogIndexFile.exists()) {
-				FileUtils.forceDelete(binlogIndexFile);
+			File binlogL1IndexFile = new File(BINLOG_L1_INDEX_ROOT_PATH, database);
+			if (binlogL1IndexFile.exists()) {
+				FileUtils.forceDelete(binlogL1IndexFile);
+			}
+
+			File binlogL2IndexFile = new File(BINLOG_L2_INDEX_ROOT_PATH, database);
+			if (binlogL2IndexFile.exists()) {
+				FileUtils.forceDelete(binlogL2IndexFile);
 			}
 
 			File binlogMasterStorageFile = new File(BINLOG_MASTER_STORAGE_ROOT_PATH, database);

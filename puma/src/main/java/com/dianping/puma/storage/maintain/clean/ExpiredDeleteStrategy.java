fb678a8e6a02fa376b91f04c6f23557bc92db4df
xiaotian.li <xiaotian.li@dianping.com>
2015-12-04 10:38:45 +0800

    add file schedulely delete

--- puma/src/main/java/com/dianping/puma/storage/maintain/clean/ExpiredDeleteStrategy.java
+++ puma/src/main/java/com/dianping/puma/storage/maintain/clean/ExpiredDeleteStrategy.java
@@ -1,5 +1,6 @@
 package com.dianping.puma.storage.maintain.clean;
 
+import com.dianping.puma.storage.utils.DateUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -7,10 +8,10 @@ import java.io.File;
 @Service
 public class ExpiredDeleteStrategy implements DeleteStrategy {
 
-	private int preservedDay;
+	private final int preservedDay = 5;
 
 	@Override
 	public boolean canClean(File directory) {
-		return false;
+		return DateUtils.expired(directory.getName(), DateUtils.getNowString(), preservedDay);
 	}
 }
--- puma/src/main/java/com/dianping/puma/storage/utils/DateUtils.java
+++ puma/src/main/java/com/dianping/puma/storage/utils/DateUtils.java
@@ -51,4 +51,11 @@ public final class DateUtils {
         DateTime date = getNow();
         return date.getYear() * 10000 + date.getMonthOfYear() * 100 + date.getDayOfMonth();
     }
+
+    public static boolean expired(String preDateStr, String postDateStr, int delta) {
+        DateTime preDateTime = DateTime.parse(preDateStr, DATE_FORMATTER);
+        DateTime postDateTime = DateTime.parse(postDateStr, DATE_FORMATTER);
+        preDateTime.withFieldAdded(DurationFieldType.days(), delta);
+        return DateTimeComparator.getDateOnlyInstance().compare(preDateTime, postDateTime) > 0;
+    }
 }

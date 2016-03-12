dcbfa3927c49972f01ee13f93d63fbc3ddb5cbfd
lixt <lixiaotian07@gmail.com>
2016-03-12 22:41:42 +0800

    add puma event model and handler skeleton

---
+++ common/src/main/java/com/dianping/puma/common/model/message/EventUnsubscribeRequest.java
@@ -0,0 +1,8 @@
+package com.dianping.puma.common.model.message;
+
+/**
+ * Created by xiaotian.li on 16/3/12.
+ * Email: lixiaotian07@gmail.com
+ */
+public class EventUnsubscribeRequest extends EventRequest {
+}

9130f3c0193da69a15aeedd7e6237408916322dd
lixt <lixiaotian07@gmail.com>
2016-03-16 16:54:24 +0800

    implement linear alarm controller

--- alarm/src/main/java/com/dianping/puma/alarm/control/LinearAlarmController.java
+++ alarm/src/main/java/com/dianping/puma/alarm/control/LinearAlarmController.java
@@ -5,12 +5,20 @@ import com.dianping.puma.alarm.exception.PumaAlarmControlUnsupportedException;
 import com.dianping.puma.common.AbstractPumaLifeCycle;
 import com.dianping.puma.common.model.alarm.result.AlarmResult;
 import com.dianping.puma.common.model.alarm.strategy.AlarmStrategy;
+import com.dianping.puma.common.utils.Clock;
+import com.google.common.collect.MapMaker;
+
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by xiaotian.li on 16/3/16.
  * Email: lixiaotian07@gmail.com
  */
 public class LinearAlarmController extends AbstractPumaLifeCycle implements PumaAlarmController {
+
+    private Clock clock;
+
+    private ConcurrentMap<String, Long> lastAlarmTimeMap = new MapMaker().makeMap();
 
     @Override
     public AlarmResult control(String clientName, AlarmResult result, AlarmStrategy strategy)
@@ -19,6 +27,31 @@ public class LinearAlarmController extends AbstractPumaLifeCycle implements Puma
             throw new PumaAlarmControlUnsupportedException("unsupported alarm strategy[%s]", strategy);
         }
 
-        return null;
+        if (!result.isAlarm()) {
+            lastAlarmTimeMap.remove(clientName);
+            return result;
+        } else {
+
+            if (!lastAlarmTimeMap.containsKey(clientName)) {
+                lastAlarmTimeMap.put(clientName, clock.getTimestamp());
+                return result;
+            } else {
+                long lastAlarmTime = lastAlarmTimeMap.get(clientName);
+                long now = clock.getTimestamp();
+                long linearAlarmIntervalInSecond = strategy.getLinearAlarmIntervalInSecond();
+                if (now - lastAlarmTime > linearAlarmIntervalInSecond) {
+                    lastAlarmTimeMap.put(clientName, now);
+                    return result;
+                } else {
+                    result.setAlarm(false);
+                    return result;
+                }
+            }
+
+        }
+    }
+
+    public void setClock(Clock clock) {
+        this.clock = clock;
     }
 }

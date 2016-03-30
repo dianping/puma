814d4b28418676ed7896d0a7bff802fb4b747e0b
lixt <lixiaotian07@gmail.com>
2016-03-30 17:55:39 +0800

    force change

--- alarm-deploy/src/main/java/com/dianping/puma/alarm/deploy/PumaAlarmMonitorServerLauncher.java
+++ alarm-deploy/src/main/java/com/dianping/puma/alarm/deploy/PumaAlarmMonitorServerLauncher.java
@@ -24,6 +24,7 @@ public class PumaAlarmMonitorServerLauncher {
     public static void main(String[] args) throws IOException {
         String springXml;
 
+
         ConfigManager configManager = ConfigManagerLoader.getConfigManager();
         if (configManager == null) {
             Properties properties = new Properties();

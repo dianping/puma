33efcc6a1a1cd1a1fa3bda7f060cd2605e211963
lixt <lixiaotian07@gmail.com>
2016-04-01 15:28:46 +0800

    try to fix http client blocking bugs

--- extensions/notify/src/main/java/com/dianping/puma/extensions/notify/PaasWeChatService.java
+++ extensions/notify/src/main/java/com/dianping/puma/extensions/notify/PaasWeChatService.java
@@ -4,11 +4,27 @@ import com.dianping.puma.alarm.exception.PumaAlarmNotifyException;
 import com.dianping.puma.alarm.service.PumaWeChatService;
 import com.google.gson.JsonObject;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.VersionInfo;
 
 /**
  * Created by xiaotian.li on 16/3/28.
@@ -18,12 +34,41 @@ public class PaasWeChatService implements PumaWeChatService {
 
     private String httpPath = "http://web.paas.dp/wechat/sendByRequestBody";
 
-    private HttpClient httpClient = HttpClients.custom()
-            .setDefaultRequestConfig(
-                    RequestConfig.custom()
-                            .setConnectTimeout(10 * 1000)
-                            .setSocketTimeout(10 * 60 * 1000)
-                            .build()).build();
+    private static final int TIMEOUT = 2000;
+
+    private static final int CONNECTION_TIMEOUT = 2000;
+
+    private static final int SOCKET_TIMEOUT = 1000;
+
+    private HttpClient httpClient;
+
+    private void createHttpClient() {
+        HttpParams params = new BasicHttpParams();
+
+        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+        HttpProtocolParams.setContentCharset(params, HTTP.DEFAULT_CONTENT_CHARSET);
+        HttpProtocolParams.setUseExpectContinue(params, true);
+        HttpConnectionParams.setTcpNoDelay(params, true);
+        HttpConnectionParams.setSocketBufferSize(params, 8192);
+
+        final VersionInfo vi = VersionInfo.loadVersionInfo("org.apache.http.client", getClass().getClassLoader());
+        final String release = (vi != null) ? vi.getRelease() : VersionInfo.UNAVAILABLE;
+        HttpProtocolParams.setUserAgent(params, "Apache-HttpClient/" + release + " (java 1.5)");
+
+        // 等待获取链接时间
+        ConnManagerParams.setTimeout(params, TIMEOUT);
+        // 链接超时时间
+        HttpConnectionParams.setConnectionTimeout(params, CONNECTION_TIMEOUT);
+        // 读取超时时间
+        HttpConnectionParams.setSoTimeout(params, SOCKET_TIMEOUT);
+
+        SchemeRegistry schemeRegistry = new SchemeRegistry();
+        schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+        schemeRegistry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
+
+        ClientConnectionManager connectionManager = new ThreadSafeClientConnManager(params, schemeRegistry);
+        httpClient = new DefaultHttpClient(connectionManager, params);
+    }
 
     @Override
     public void send(String recipient, String message) {
